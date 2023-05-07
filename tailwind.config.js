8186b93ade57c74d038c16102fbd63204439abec
DamianGrosz2 <94179814+DamianGrosz2@users.noreply.github.com>
2023-05-07 09:32:06 +0200

    FRONT-45: Add Styleguide (#9)
    closes FRONT-45: Add Styleguide
    
    ---------
    
    Co-authored-by: Damian Gross <boote_pints_0m@icloud.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,20 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      }
+      },
+      colors: {
+        'bg-primary': '#191A23',
+        'text-primary': '#EEEFFC',
+        'border-color': '#2C2D3C',
+        'text-secondary': '#D2D3E0',
+        'bg-secondary': '#1F2130',
+        'icon-color': '#858699',
+        'bg-secondary-highlighted': '#26273B',
+        'highlight-color': '#575BC7',
+        'red': '#EB5757',
+        'yellow': '#F2C94C',
+        'green': '#4CB782'
+      },
     }
   },
   plugins: []

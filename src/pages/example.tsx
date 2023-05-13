33bec17b73042f0ea2690228246d5ee806f07d32
Clemens <cnk1dev@gmail.com>
2023-05-14 00:49:14 +0200

    Add simple page demonstrating how to use graphql

---
+++ src/pages/example.tsx
@@ -0,0 +1,22 @@
+import { useGitRepositoriesQuery } from '@/graphql/repositories.gql'
+
+/**
+ * Simple component demonstrating how to use GraphQL queries
+ */
+export default function Example() {
+  const [{ data, fetching, error }] = useGitRepositoriesQuery()
+
+  if (fetching) return <p>Loading...</p>
+  if (error) return <p>Oh no... {error.message}</p>
+
+  return (
+    <div>
+      {data?.gitRepositories.map((repository) => (
+        <div key={repository?.id}>
+          <h1>{repository?.name}</h1>
+          <p>{repository?.stars}</p>
+        </div>
+      ))}
+    </div>
+  )
+}

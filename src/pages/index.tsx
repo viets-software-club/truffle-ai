fe05961930785e90bec8728e127d59035e13ac3c
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-05-15 12:41:05 +0200

    Spotlight impl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,33 @@
 import { Inter } from 'next/font/google'
 import RepositoryTable from '@/components/RepositoryTable'
+import Spotlight from '@/components/Spotlight/spotlight'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Home = () => (
-  <main className={`min-h-screen p-24 ${inter.className} bg-primary text-primary`}>
-    <RepositoryTable />
-  </main>
-)
+const Home = () => {
+  const [openModel, setOpenModel] = useState(false)
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.ctrlKey && event.key === 'l') {
+        setOpenModel(true)
+      } else if (event.key === 'Escape') {
+        setOpenModel(false)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  return (
+    <main className={`min-h-screen p-24 ${inter.className} bg-primary text-primary`}>
+      <RepositoryTable />
+      {openModel && <Spotlight />}
+    </main>
+  )
+}
+
 export default Home

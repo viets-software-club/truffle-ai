cbfdec9eff961a7a5f956ef623d048a4fc574ec9
thomas-woehrle <127286604+thomas-woehrle@users.noreply.github.com>
2023-05-15 10:47:57 +0200

    BACK-28: Implement endpoint serving mock data (#9)
    * Initial commit
    
    * Add schema for a project.
    
    * Add resolvers. Add one sample project.
    
    * Resolve issue with interface resolver. Add SocialMediaTopPost resolver.
    
    * Delete old examplatory data. Move sample project to json file.
    
    * Add multiple sample projects.
    
    * Adapt code comments.

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,15 +1,111 @@
 // see https://graphql.org/learn/schema/
 const schema = `
-	type GitRepository {
-		id: ID!
-		name: String!
-		stars: Int!
-		contributor: [String]!
-	}
+  type Project {
+    id: ID!
+    gitHubInformation: GitHubInformation!
+    socialMediaAccounts: [SocialMediaPresence]!
+    socialMediaTopPosts: [SocialMediaTopPost]!
+  }
 
+  type GitHubInformation {
+    name: String!
+    author: GitHubUser!
+    about: String!
+    eli5: String!
+    programmingLanguages: [String]!
+    categories: [String]!
+    starCount: Int!
+    issueCount: Int!
+    forkCount: Int!
+    pullRequestCount: Int!
+    contributorsCount: Int!
+    topThreeContributors: [GitHubUser]!
+    linkToRepo: String!
+    websiteOfRepo: String!
+  }
+
+  type GitHubUser {
+    gitHubHandle: String!
+    name: String!
+    repositoryCount: Int!
+    starCount: Int!
+    employedAt: String
+    email: String
+    personalWebsite: String
+    twitterHandle: String
+  }
+
+  interface SocialMediaPresence {
+    platform: String!
+    accountName: String!
+    link: String!
+  }
+
+  type TwitterAccount implements SocialMediaPresence {
+    platform: String!
+    accountName: String!
+    link: String!
+    followerCount: Int!
+  }
+
+  type LinkedInAccount implements SocialMediaPresence {
+    platform: String!
+    accountName: String!
+    link: String!
+    followerCount: Int!
+  }
+
+  type SlackChannel implements SocialMediaPresence {
+    platform: String!
+    accountName: String!
+    link: String!
+    memberCount: Int!
+  }
+
+  type DiscordChannel implements SocialMediaPresence {
+    platform: String!
+    accountName: String!
+    link: String!
+    memberCount: Int!
+  }
+
+  interface SocialMediaTopPost {
+    platform: String!
+    link: String!
+    title: String!
+  }
+
+  type HackernewsTopPost implements SocialMediaTopPost {
+    platform: String!
+    link: String!
+    title: String!
+    pointCount: Int!
+    commentCount: Int!
+  }
+
+  type TwitterTopPost implements SocialMediaTopPost {
+    platform: String!
+    link: String!
+    title: String!
+    date: String!
+    likeCount: Int!
+    commentCount: Int!
+    retweetCount: Int!
+    viewsCount: Int!
+  }
+
+  type ProductHuntTopPost implements SocialMediaTopPost {
+    platform: String!
+    link: String!
+    title: String!
+    descriptionPreview: String!
+    date: String!
+    likeCount: Int!
+    commentCount: Int!
+  }
+  
   type Query {
-    gitRepository(id: ID!): GitRepository!
-		gitRepositories: [GitRepository]!
+    projects: [Project!]!
   }
 `
 

74a6b2fe7c34090c81e89a0677bae2d204820c8e
Philipp Czernitzki <i@pczern.dev>
2023-05-18 20:54:24 +0200

    Update Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,3 @@
-import { ComponentType, useMemo } from 'react'
-
 type ButtonProps = {
   variant:
     | 'normal'
@@ -11,60 +9,47 @@ type ButtonProps = {
     | 'normalHighlighted'
   onClick: () => void
   text?: string
-  Icon?: ComponentType<{ className?: string }>
-  switchOrder?: boolean
+  Icon?: IconComponentType
+  order?: 'ltr' | 'rtl'
   iconColor?: string
   textColor?: string
   fullWidth?: boolean
 }
+
+const baseClassNames = `flex items-center rounded-[5px] transition-colors duration-100 hover:bg-gray-700`
+
+const variantToButtonVariantClassNames = new Map<ButtonProps['variant'], string>([
+  ['normal', 'bg-gray-850 border border-gray-800 px-4 py-2'],
+  ['normalHighlighted', 'bg-gray-700 border border-gray-800 px-4 py-2'],
+  ['noBG', 'border border-gray-800 px-4 py-2'],
+  ['noBordernoBG', ' px-4 py-2'],
+  ['highlighted', 'bg-indigo-500 px-4 py-2'],
+  ['onlyIcon', 'bg-gray-850 border border-gray-800 px-1.5 py-1.5'],
+  ['onlyIconnoBordernoBG', '']
+])
 
 const Button = ({
   variant,
   onClick,
   text,
   Icon,
-  switchOrder,
+  order,
   iconColor = 'text-gray-500',
   textColor = 'text-gray-300',
   fullWidth
 }: ButtonProps) => {
-  const contentLayout = useMemo(() => {
-    if (Icon && !text) {
-      return <Icon className={`h-4 w-4 ${iconColor}`} />
-    }
-    return switchOrder ? (
-      <>
-        {text && <span className={`text-14 ${textColor}`}>{text}</span>}
-        {Icon && <Icon className={`ml-2 h-4 w-4 ${iconColor}`} />}
-      </>
-    ) : (
-      <>
-        {Icon && <Icon className={`mr-2 h-4 w-4 ${iconColor}`} />}
-        {text && <span className={`text-14 ${textColor}`}>{text}</span>}
-      </>
-    )
-  }, [Icon, text, switchOrder, iconColor, textColor])
+  if (Icon && !text) return <Icon className={`h-4 w-4 ${iconColor}`} />
 
-  const variantToButtonStyle = new Map([
-    ['normal', 'bg-gray-850 border border-gray-800 px-4 py-2'],
-    ['normalHighlighted', 'bg-gray-700 border border-gray-800 px-4 py-2'],
-    ['noBG', 'border border-gray-800 px-4 py-2'],
-    ['noBordernoBG', ' px-4 py-2'],
-    ['highlighted', 'bg-indigo-500 px-4 py-2'],
-    ['onlyIcon', 'bg-gray-850 border border-gray-800 px-1.5 py-1.5'],
-    ['onlyIconnoBordernoBG', '']
-  ])
-
-  let buttonStyle = variantToButtonStyle.get(variant) || ''
-  buttonStyle += fullWidth ? ' w-full' : ''
+  const classNames = `${baseClassNames} ${variantToButtonVariantClassNames.get(variant) ?? ''} ${
+    fullWidth ? 'w-full' : ''
+  }`
+  const textNode = text && <span className={`text-14 ${textColor}`}>{text}</span>
+  const iconNode = Icon && <Icon className={`mr-2 h-4 w-4 ${iconColor}`} />
+  const contentNode = order === 'ltr' ? [iconNode, textNode] : [textNode, iconNode]
 
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      className={`flex items-center rounded-[5px] transition-colors duration-100 hover:bg-gray-700 ${buttonStyle}`}
-    >
-      {contentLayout}
+    <button type="button" onClick={onClick} className={classNames}>
+      {contentNode}
     </button>
   )
 }
@@ -72,7 +57,7 @@ const Button = ({
 Button.defaultProps = {
   text: null,
   Icon: null,
-  switchOrder: false,
+  order: 'rtl',
   iconColor: 'text-gray-500',
   textColor: 'text-gray-300',
   fullWidth: false

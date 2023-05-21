3de43d3165692eb0996b768c5a4e9be32e980349
Philipp Czernitzki <i@pczern.dev>
2023-05-21 18:20:45 +0200

    Remove footer from Repository table

--- src/pureComponents/RepositoryTable/index.tsx
+++ src/pureComponents/RepositoryTable/index.tsx
@@ -150,20 +150,6 @@ const RepositoryTable = () => {
             </tr>
           ))}
         </tbody>
-        {/* @TODO We are not using Footers for now */}
-        {/* <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(header.column.columnDef.footer, header.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot> */}
       </table>
     </div>
   )

488dce33f9f4f6d745f091d83683304aeddd0f20
Arno Claude <arno.claude@googlemail.com>
2023-05-23 09:40:44 +0200

    moved logout out of login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,7 @@ import { withUrqlClient } from 'next-urql'
 import { AppProps } from 'next/app'
 import BASE_URL from '@/constants/baseUrl'
 import '@/styles/globals.css'
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 
--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,22 +1,16 @@
 import { Inter } from 'next/font/google'
 import Button from '@/components/pure/Button'
-import { AiOutlineGoogle } from 'react-icons/ai'
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
-const Login = () => {
+const Logout = () => {
   const user = useUser()
   const supabaseClient = useSupabaseClient()
   const [isError, setIsError] = useState(false)
 
-  async function signInWithGoogle() {
-    const { error } = await supabaseClient.auth.signInWithOAuth({
-      provider: 'google'
-    })
-    if (error) {
-      setIsError(true)
-    }
+  async function signOut() {
+    await supabaseClient.auth.signOut().catch(() => setIsError(true))
   }
 
   if (isError) {
@@ -27,8 +21,7 @@ const Login = () => {
           <div className="flex flex-col items-center space-y-4">
             <div className="mb-4 text-36 font-semibold text-gray-100">Welcome to TruffleAI</div>
             <div>
-              There was an error signing you in with Google. Please try again later or contact an
-              admin.
+              There was an error signing you out. Please try again later or contact an admin.
             </div>
           </div>
           <div className="self-center pb-4 text-12 text-gray-300">© 2023 La Famiglia x Rostlab</div>
@@ -44,19 +37,16 @@ const Login = () => {
         <div className="flex flex-col items-center space-y-4">
           <div className="mb-4 text-36 font-semibold text-gray-100">Welcome to TruffleAI</div>
           {user ? (
-            <div>You are already logged in.</div>
-          ) : (
             <Button
-              text="Continue with Google"
-              Icon={AiOutlineGoogle}
-              order="ltr"
-              iconColor="white"
+              text="Logout"
               textColor="white"
               onClick={() => {
-                signInWithGoogle().catch(() => setIsError(true))
+                signOut().catch(() => setIsError(true))
               }}
-              variant="highlighted"
+              variant="normal"
             />
+          ) : (
+            <div>You are already logged out.</div>
           )}
         </div>
         <div className="self-center pb-4 text-12 text-gray-300">© 2023 La Famiglia x Rostlab</div>
@@ -65,4 +55,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Logout

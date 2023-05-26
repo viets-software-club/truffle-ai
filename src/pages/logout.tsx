0a9498f059bbae2227f1a8031d8a2eae3098c3ff
Philipp Czernitzki <i@pczern.dev>
2023-05-26 20:21:04 +0200

    Update logout page

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,19 +1,13 @@
-import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const Logout = () => {
-  const user = useUser()
   const supabaseClient = useSupabaseClient()
   const router = useRouter()
 
-  const signOut = async () => {
-    await supabaseClient.auth.signOut()
-  }
-
   useEffect(() => {
-    if (user) void signOut().then(() => router.replace('/login'))
-    else void router.replace('/login')
+    void supabaseClient.auth.signOut().then(() => router.replace('/login'))
   }, [])
 }
 

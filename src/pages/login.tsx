713349d9e0576f2027317b38c7ad3f2c4e063a81
Clemens <cnk1dev@gmail.com>
2023-05-27 17:37:18 +0200

    Make error message separate component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 import { Inter } from 'next/font/google'
-import Button from '@/components/pure/Button'
 import { AiOutlineGoogle } from 'react-icons/ai'
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
+import Button from '@/components/pure/Button'
+import Error from '@/components/pure/Error'
 
 const inter = Inter({ subsets: ['latin'] })
+
 const Login = () => {
   const user = useUser()
   const supabaseClient = useSupabaseClient()
@@ -23,46 +25,32 @@ const Login = () => {
     signInWithGoogle().catch(() => setIsError(true))
   }
 
-  if (isError) {
-    return (
-      <main className={`${inter.className} flex min-h-screen flex-col`}>
+  return (
+    <main className={`${inter.className} flex min-h-screen flex-col`}>
+      {isError ? (
+        <Error title="Error" message="Something went wrong. Please try again." />
+      ) : (
         <div className="flex grow flex-col items-center justify-between bg-radial-gradient">
           <div />
           <div className="flex flex-col items-center space-y-4">
-            <div className="mb-4 text-36 font-semibold text-gray-100">Something went wrong</div>
-            <div>
-              There was an error signing you in with Google. Please try again later or contact an
-              admin.
-            </div>
+            <div className="mb-4 text-36 font-semibold text-gray-100">Welcome to TruffleAI</div>
+            {user ? (
+              <p>You are already logged in.</p>
+            ) : (
+              <Button
+                text="Continue with Google"
+                Icon={AiOutlineGoogle}
+                order="ltr"
+                iconColor="white"
+                textColor="white"
+                onClick={handleSignInWithGoogle}
+                variant="highlighted"
+              />
+            )}
           </div>
           <div className="self-center pb-4 text-12 text-gray-300">© 2023 La Famiglia x Rostlab</div>
         </div>
-      </main>
-    )
-  }
-
-  return (
-    <main className={`${inter.className} flex min-h-screen flex-col`}>
-      <div className="flex grow flex-col items-center justify-between bg-radial-gradient">
-        <div />
-        <div className="flex flex-col items-center space-y-4">
-          <div className="mb-4 text-36 font-semibold text-gray-100">Welcome to TruffleAI</div>
-          {user ? (
-            <p>You are already logged in.</p>
-          ) : (
-            <Button
-              text="Continue with Google"
-              Icon={AiOutlineGoogle}
-              order="ltr"
-              iconColor="white"
-              textColor="white"
-              onClick={handleSignInWithGoogle}
-              variant="highlighted"
-            />
-          )}
-        </div>
-        <div className="self-center pb-4 text-12 text-gray-300">© 2023 La Famiglia x Rostlab</div>
-      </div>
+      )}
     </main>
   )
 }

eaecb4448c2a8ad88c16c32769f98c817774d228
Clemens <cnk1dev@gmail.com>
2023-05-28 23:05:15 +0200

    Update loading component

---
+++ src/components/pure/Loading.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Logo from '@/assets/logo.svg'
+
+type LoadingProps = {
+  message?: string
+}
+
+/**
+ * Simple loading component with logo and customizable message
+ */
+const Loading = ({ message }: LoadingProps) => (
+  <div className="flex h-screen grow flex-col items-center justify-between">
+    <div />
+
+    <div className="flex flex-col items-center">
+      {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+      <Image src={Logo} alt="logo" className="h-10 w-10" />
+      <p className="pt-6">{message || 'Loading...'}</p>
+    </div>
+
+    <div className="self-center pb-4 text-12 text-gray-300">© 2023 La Famiglia x Rostlab</div>
+  </div>
+)
+
+Loading.defaultProps = {
+  message: undefined
+}
+
+export default Loading

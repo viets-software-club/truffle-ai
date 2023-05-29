38f664d84cd7b2f4cd33a848b4117909eaccf75b
Clemens <cnk1dev@gmail.com>
2023-05-29 22:25:05 +0200

    Fix build error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,7 +35,7 @@ export default withUrqlClient(
     exchanges: [cacheExchange, fetchExchange],
     fetchOptions: () => ({
       headers: {
-        Authorization: document.cookie
+        Authorization: typeof window !== 'undefined' ? document.cookie : ''
       }
     })
   }),

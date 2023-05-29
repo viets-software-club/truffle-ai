0063f1691710292d72f5661c247128510c868142
Lukas <ge64diw@mytum.de>
2023-05-29 15:51:50 +0200

    add time modal to Edit Timeframe button and add changes to Modal to fix the modals under the button

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -21,6 +21,8 @@ import {
 import Page from '@/components/side-effects/Page'
 import SmallSidebar from '@/components/page/details/Sidebar'
 import withAuth from '@/components/side-effects/withAuth'
+import Modal from '@/components/pure/Modal'
+import { useState } from 'react'
 
 const handleClick = () => {
   // eslint-disable-next-line no-alert
@@ -29,47 +31,83 @@ const handleClick = () => {
 const SomeButton = (
   <Button Icon={FiArrowUpRight} variant="normal" onClick={handleClick} text="Open" order="ltr" />
 )
-const DetailPage = () => (
-  <Page>
-    <div className="border-b border-solid border-gray-800 py-2.5 pl-7 pr-2 text-gray-500">
-      <div className="flex justify-between">
-        <div className="flex flex-row items-center gap-3">
-          <Button variant="onlyIconNoBorderNoBG" onClick={handleClick} Icon={X} />
-          <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronUp} />
-          <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronDown} />
-          <p className="text-14 text-gray-300">3/12</p>
+
+const DetailPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <Page>
+      <div className="border-b border-solid border-gray-800 py-2.5 pl-7 pr-2 text-gray-500">
+        <div className="flex justify-between">
+          <div className="flex flex-row items-center gap-3">
+            <Button variant="onlyIconNoBorderNoBG" onClick={handleClick} Icon={X} />
+            <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronUp} />
+            <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronDown} />
+            <p className="text-14 text-gray-300">3/12</p>
+          </div>
+          <div className="flex flex-col">
+            <Button
+              variant="normal"
+              onClick={handleOpenModal}
+              text="Edit timeframe"
+              Icon={Calendar}
+              order="ltr"
+            />
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+              <Button variant="noBorderNoBG" text="Today" fullWidth onClick={() => handleClick} />
+              <Button
+                variant="noBorderNoBG"
+                text="This Week"
+                fullWidth
+                onClick={() => handleClick}
+              />
+              <Button
+                variant="noBorderNoBG"
+                text="This Month"
+                fullWidth
+                onClick={() => handleClick}
+              />
+            </Modal>
+          </div>
         </div>
-        <Button
-          variant="normal"
-          onClick={handleClick}
-          text="Edit timeframe"
-          Icon={Calendar}
-          order="ltr"
-        />
       </div>
-    </div>
-    <div className="flex grow">
-      <div className="w-4/5 flex-row">
-        <ProjectInformation
-          image=""
-          name="vercel/next.js"
-          eli5="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-          tags={['React', 'Static Site Generation', 'TypeScript']}
-        />
-        <Chart data={data} />
-        {/* <ScatterPlot /> */}
-        <div className="flex flex-row gap-3 px-3">
-          <Card Icon={FaTwitter} name="Top Tweets" button={SomeButton} textFields={tweetListMock} />
-          <Card
-            Icon={FaHackerNews}
-            name="Community sentiment"
-            button={SomeButton}
-            textFields={hackerNewsListMock}
+
+      <div className="flex grow">
+        <div className="w-4/5 flex-row">
+          <ProjectInformation
+            image=""
+            name="vercel/next.js"
+            eli5="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            tags={['React', 'Static Site Generation', 'TypeScript']}
           />
+          <Chart data={data} />
+          {/* <ScatterPlot /> */}
+          <div className="flex flex-row gap-3 px-3">
+            <Card
+              Icon={FaTwitter}
+              name="Top Tweets"
+              button={SomeButton}
+              textFields={tweetListMock}
+            />
+            <Card
+              Icon={FaHackerNews}
+              name="Community sentiment"
+              button={SomeButton}
+              textFields={hackerNewsListMock}
+            />
+          </div>
         </div>
+        <SmallSidebar />
       </div>
-      <SmallSidebar />
-    </div>
-  </Page>
-)
+    </Page>
+  )
+}
 export default withAuth(DetailPage)

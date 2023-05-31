08c2f5e96d14df85151260d716a1986c1964bf30
Clemens <cnk1dev@gmail.com>
2023-05-31 23:08:54 +0200

    Fix sidebar styling

--- src/components/pure/Sidebar/Section/index.tsx
+++ src/components/pure/Sidebar/Section/index.tsx
@@ -11,7 +11,7 @@ const Section = ({ children, title }: SectionProps) => (
     key="div-2"
     className="border-t border-solid border-gray-800 py-2.5 text-14 font-normal leading-4 text-gray-100"
   >
-    <p className="px-7 py-2.5 text-10 font-medium uppercase text-gray-400">{title}</p>
+    <p className="px-7 py-2.5 text-12 font-medium uppercase text-gray-500">{title}</p>
     <div className="px-2">{children}</div>
   </div>
 )

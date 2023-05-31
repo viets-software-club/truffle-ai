4d6954662ed77aee0ca7cf3a9e6aae7d0f236192
Clemens <cnk1dev@gmail.com>
2023-05-31 23:13:46 +0200

    Format large numbers in table + shorten long column titles

--- src/components/pure/ProjectsTable/columns.tsx
+++ src/components/pure/ProjectsTable/columns.tsx
@@ -5,6 +5,7 @@ import { BsPeople } from 'react-icons/bs'
 import { VscIssues } from 'react-icons/vsc'
 import GitHubStatisticItem from '@/components/pure/Sidebar/Box/GithubStatItem'
 import { Project } from '@/generated/gql'
+import formatNumber from '@/util/formatNumber'
 import Logo from '@/assets/logo.svg'
 
 const columnHelper = createColumnHelper<Project>()
@@ -17,7 +18,7 @@ const columns = [
     // @TODO Add real logo
     // @TODO Fix next image types
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    cell: () => <Image src={Logo} alt="logo" className="h-6 w-6" />
+    cell: () => <Image src={Logo} alt="logo" className="ml-2 h-5 w-5" />
   }),
   // @TODO Adjust for user owners
   columnHelper.accessor(({ organization, name }) => `${organization?.login || 'user'} / ${name}`, {
@@ -63,7 +64,7 @@ const columns = [
     )
   }),
   columnHelper.accessor('contributorCount', {
-    header: 'Contributors',
+    header: 'Contrib.',
     cell: (info) => (
       <GitHubStatisticItem
         Icon={BsPeople}
@@ -76,13 +77,13 @@ const columns = [
   }),
   columnHelper.accessor((project) => (project.forkCount || 0) / (project.contributorCount || 1), {
     id: 'forksPerContributor',
-    header: 'Forks/ Contributor',
-    cell: (info) => <p className="text-14 font-bold">{info.getValue()}</p>
+    header: 'Forks/ Contr.',
+    cell: (info) => <p className="text-14">{formatNumber(info.getValue())}</p>
   }),
   columnHelper.accessor((project) => (project.issueCount || 0) / (project.contributorCount || 1), {
     id: 'issuesPerContributor',
-    header: 'Issues/ Contributor',
-    cell: (info) => <p className="text-14 font-bold">{info.getValue()}</p>
+    header: 'Issues/ Contr.',
+    cell: (info) => <p className="text-14">{formatNumber(info.getValue())}</p>
   })
 ]
 

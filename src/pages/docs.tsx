5fd88647f689d79e70585a25ff76074d533bc21c
Clemens <cnk1dev@gmail.com>
2023-05-31 23:06:43 +0200

    Update docs page

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -3,7 +3,10 @@ import withAuth from '@/components/side-effects/withAuth'
 
 const Docs = () => (
   <Page>
-    <div className="p-7 ">Documentation</div>
+    <div className="flex h-[60px] flex-row items-center justify-between border-b border-gray-800 px-6">
+      <h1 className="text-20 font-medium">Documentation</h1>
+    </div>
   </Page>
 )
+
 export default withAuth(Docs)

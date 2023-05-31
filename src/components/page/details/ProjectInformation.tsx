a92471f898257ce851bed891fee5880b72c3ad35
Clemens <cnk1dev@gmail.com>
2023-05-31 23:16:13 +0200

    Fix detail page styling

--- src/components/page/details/ProjectInformation.tsx
+++ src/components/page/details/ProjectInformation.tsx
@@ -1,4 +1,4 @@
-import { StarIcon } from '@primer/octicons-react'
+import { FiBookmark as Bookmark } from 'react-icons/fi'
 import Button from '@/components/pure/Button'
 
 type ProjectInformationProps = {
@@ -13,19 +13,19 @@ type ProjectInformationProps = {
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 const ProjectInformation = ({ image, name, eli5, tags }: ProjectInformationProps) => (
-  <div className="border-b border-gray-800 px-7 py-4">
-    <div className="mb-4 flex flex-row items-center justify-between">
+  <div className="border-b border-gray-800 px-7 py-6">
+    <div className="mb-6 flex flex-row items-center justify-between">
       <div className="flex flex-row items-center">
         {/* @TODO Replace with actual image */}
-        <div className="mr-4 h-[30px] w-[30px] rounded-[5px] bg-gray-500" />
+        <div className="mr-4 h-[30px] w-[30px] rounded-[5px] bg-gray-600" />
 
-        <h1 className="mr-3 text-16 font-medium">{name}</h1>
+        <h1 className="mr-3 text-20 font-medium">{name}</h1>
 
         {tags.length > 0 ? (
           tags.map((text) => (
             <p
               key={text}
-              className="mx-1 rounded-lg bg-gray-850 px-2 py-0.5 text-12 font-light text-gray-300"
+              className="mx-1 rounded-[5px] bg-gray-850 px-2 py-0.5 text-12 font-light text-gray-300"
             >
               {text}
             </p>
@@ -36,19 +36,18 @@ const ProjectInformation = ({ image, name, eli5, tags }: ProjectInformationProps
       </div>
 
       <Button
-        variant="highlighted"
+        variant="normal"
         text="Bookmark"
-        Icon={StarIcon}
+        Icon={Bookmark}
         onClick={() => {
           // @TODO Implement bookmark functionality
         }}
         order="ltr"
         textColor="text-gray-100"
-        iconColor="text-gray-100"
       />
     </div>
 
-    <p className="text-12 font-light">{eli5}</p>
+    <p className="text-14 font-light">{eli5}</p>
   </div>
 )
 
--- src/components/side-effects/Details/index.tsx
+++ src/components/side-effects/Details/index.tsx
@@ -57,46 +57,47 @@ const Details = ({ id }: DetailsProps) => {
 
   return (
     <>
-      <div className="px-3 py-2 pl-7 text-gray-500">
-        <div className="flex justify-between">
-          <div className="flex flex-row items-center gap-3">
-            <Link href="/">
-              <X key="2" className="h-4 w-4 text-gray-500" />
-            </Link>
+      <div className="flex h-[59px] w-full items-center justify-between px-3 pl-7 text-gray-500">
+        <div className="flex flex-row items-center gap-3">
+          <Link href="/">
+            <X key="2" className="h-4 w-4 text-gray-500" />
+          </Link>
 
-            <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronUp} />
-            <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronDown} />
+          <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronUp} />
+          <Button variant="onlyIcon" onClick={handleClick} Icon={ChevronDown} />
 
-            {/* @TODO Make values dynamic */}
-            <p className="text-14 text-gray-300">3/12</p>
+          {/* @TODO Make values dynamic */}
+          <div className="flex flex-row items-center">
+            <p className="text-14 text-white">10&nbsp;</p>
+            <p className="text-14 text-gray-500">/&nbsp;25</p>
           </div>
+        </div>
 
-          <div className="flex flex-col">
-            <Button
-              variant="normal"
-              onClick={handleOpenModal}
-              text="Edit timeframe"
-              Icon={Calendar}
-              order="ltr"
-            />
+        <div className="flex flex-col">
+          <Button
+            variant="normal"
+            onClick={handleOpenModal}
+            text="This week"
+            Icon={Calendar}
+            order="ltr"
+          />
 
-            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-              <Button variant="noBorderNoBG" text="Today" fullWidth onClick={handleClick} />
-              <Button variant="noBorderNoBG" text="This Week" fullWidth onClick={handleClick} />
-              <Button variant="noBorderNoBG" text="This Month" fullWidth onClick={handleClick} />
-            </Modal>
-          </div>
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+            <Button variant="noBorderNoBG" text="Today" fullWidth onClick={handleClick} />
+            <Button variant="noBorderNoBG" text="This Week" fullWidth onClick={handleClick} />
+            <Button variant="noBorderNoBG" text="This Month" fullWidth onClick={handleClick} />
+          </Modal>
         </div>
       </div>
 
       <div className="flex grow">
-        <div className="w-4/5 flex-row border-y border-solid border-gray-800 ">
+        <div className="w-4/5 flex-row border-t border-solid border-gray-800">
           <ProjectInformation
             // @TODO Add actual image URL
             image={project?.organization?.avatarUrl as string}
             // @TODO Adjust for owner (could be user or organization)
             name={`${project.organization?.login || 'No name'}/${project.name}`}
-            eli5={project.about || 'No description'}
+            eli5={project.eli5 || project.about || 'No description'}
             // @TODO Replace with actual tags
             tags={['React', 'Static Site Generation', 'TypeScript']}
           />
@@ -104,7 +105,7 @@ const Details = ({ id }: DetailsProps) => {
           <Chart data={chartDataMock} />
 
           {/* @TODO Add real data */}
-          <div className="my-4 flex flex-row gap-4 pl-7 pr-3">
+          <div className="flex flex-row gap-4 border-t border-solid border-gray-800 py-2 pl-7 pr-3">
             <Card
               Icon={FaTwitter}
               name="Top Tweets"

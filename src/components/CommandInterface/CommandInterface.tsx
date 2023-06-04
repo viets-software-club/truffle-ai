3a93cbd3c6a2ce317c4f62d42c030fd031bb4c49
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-06-04 09:17:48 +0300

    input ref check fixed

--- src/components/CommandInterface/CommandInterface.tsx
+++ src/components/CommandInterface/CommandInterface.tsx
@@ -8,7 +8,7 @@ const CommandInterface: React.FC<CommandInterfaceProps> = ({ action }) => {
   const [recommendationIsOpen, setRecommendationIsOpen] = React.useState<boolean>(false)
   const inputRef: RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null)
   useEffect(() => {
-    if (inputRef && inputRef.current) {
+    if (inputRef?.current) {
       inputRef.current.focus()
     }
   }, [])

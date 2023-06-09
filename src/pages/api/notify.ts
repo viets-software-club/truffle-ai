8a87824e5ecfccd4edfa5ca9edf702b1e3359800
DamianGrosz2 <94179814+DamianGrosz2@users.noreply.github.com>
2023-06-09 10:28:26 +0200

    Update src/pages/api/notify.ts
    Co-authored-by: Clemens <37487191+kigadye@users.noreply.github.com>

--- src/pages/api/notify.ts
+++ src/pages/api/notify.ts
@@ -19,7 +19,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(200).json({ success: true })
     }
   } catch (err) {
-    console.log('ERROR:', err)
     res.status(500).json({ success: false })
   }
 }

5791db353eb6bcb11c2e85540d3bcda2cbf8989e
Clemens <37487191+kigadye@users.noreply.github.com>
2023-06-11 13:07:46 +0200

    Fix table/ edit columns bug (#56)

--- src/components/pure/ProjectsTable/index.tsx
+++ src/components/pure/ProjectsTable/index.tsx
@@ -1,11 +1,12 @@
-import { useReactTable, getCoreRowModel } from '@tanstack/react-table'
-import { Project, useTrendingProjectsQuery } from '@/graphql/generated/gql'
+import { useEffect, useState } from 'react'
+import { useReactTable, getCoreRowModel, ColumnOrderState } from '@tanstack/react-table'
 import Error from '@/components/pure/Error'
 import Loading from '@/components/pure/Loading'
 import Table from '@/components/page/overview/Table'
 import TopBar from '@/components/page/overview/TopBar'
-import columns from '@/components/pure/ProjectsTable/columns'
+import defaultColumns from '@/components/pure/ProjectsTable/columns'
 import FilterBar from '@/components/page/overview/Filterbar'
+import { Project, useTrendingProjectsQuery } from '@/graphql/generated/gql'
 
 const nullFunc = () => null
 
@@ -13,20 +14,37 @@ const nullFunc = () => null
  * Table for displaying trending projects
  */
 const ProjectsTable = () => {
+  const [data, setData] = useState<Project[]>([])
+  const [columns] = useState(() => [...defaultColumns])
+  const [columnVisibility, setColumnVisibility] = useState({})
+  const [columnOrder, setColumnOrder] = useState<ColumnOrderState>([])
+
   // Fetch data from Supabase using generated Urql hook
-  const [{ data, fetching, error }] = useTrendingProjectsQuery()
-  const projects = data?.projectCollection?.edges?.map((edge) => edge.node) as Project[]
+  const [{ data: urqlData, fetching, error }] = useTrendingProjectsQuery()
+
+  // Only update table data when urql data changes
+  useEffect(() => {
+    if (urqlData) {
+      setData(urqlData?.projectCollection?.edges?.map((edge) => edge.node) as Project[])
+    }
+  }, [urqlData])
 
   // Initialize TanStack table
   const table = useReactTable({
-    data: projects,
+    data,
     columns,
+    state: {
+      columnVisibility,
+      columnOrder
+    },
+    onColumnVisibilityChange: setColumnVisibility,
+    onColumnOrderChange: setColumnOrder,
     getCoreRowModel: getCoreRowModel()
   })
 
   // Display loading/ error messages conditionally
   if (fetching) return <Loading message="Getting trending projects for you..." />
-  if (!projects || projects.length === 0 || error) return <Error />
+  if (data.length === 0 || error) return <Error />
 
   return (
     <div className="flex w-full flex-col">

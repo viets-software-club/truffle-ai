50d411ff7a8247e25940db31e2d6767afa9a09c9
Philipp Czernitzki <i@pczern.dev>
2023-06-14 13:57:05 +0200

    add logging to init-deployment.js, remove single quote

--- .github/init-deployment.js
+++ .github/init-deployment.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const deploymentFile = path.join(__dirname, '../config/deployment.yml')
 try {
+  console.log(`${process.env.COMMIT_TAG} ${process.env.COMMIT_MESSAGE.replaceAll("'", '')}`)
   const data = fs
     .readFileSync(deploymentFile, 'utf8')
     .replace('<GRAPHQL-SERVER-IMAGE>', process.env.GRAPHQL_SERVER_IMAGE)
@@ -9,11 +10,12 @@ try {
     .replace('<REPO-JOB-IMAGE>', process.env.REPO_JOB_IMAGE)
     .replace(
       '<CHANGE-CAUSE>',
-      `'${process.env.COMMIT_TAG} ${process.env.COMMIT_MESSAGE.replaceAll("'", '')}'`
+      `${process.env.COMMIT_TAG} ${process.env.COMMIT_MESSAGE.replaceAll("'", '')}`
     )
     .replace('<GRAPHQL-GATEWAY-PORT', process.env.GRAPHQL_GATEWAY_PORT)
     .replace('<GRAPHQL-SERVER-PORT>', process.env.GRAPHQL_SERVER_PORT)
     .replace('<CERTIFICATE-ID>', process.env.CERTIFICATE_ID)
+  console.log(data)
   fs.writeFileSync(deploymentFile, data)
   console.log('Deployment file written')
 } catch (error) {

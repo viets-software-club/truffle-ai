87dee857f22730d373706cba23aee882e772f4e4
Tom Woehrle <thomas.e.woehrle@gmail.com>
2023-06-14 14:04:46 +0200

    Adjust linkedIn scraping return

--- packages/graphql_server/src/test/linkedInScraping.test.ts
+++ packages/graphql_server/src/test/linkedInScraping.test.ts
@@ -1,3 +1,3 @@
 import { getCompanyInfosFromLinkedIn } from '../scraping/linkedInScraping'
 
-void getCompanyInfosFromLinkedIn('huggingface', 15000).then((r) => console.log(r))
+void getCompanyInfosFromLinkedIn('google', 25000).then((r) => console.log(r))

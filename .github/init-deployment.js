9b523d46cf6cbab747589c82662d4caa8e1507c6
Philipp Czernitzki <i@pczern.dev>
2023-06-14 12:54:29 +0200

    update init-deployment.js

--- .github/init-deployment.js
+++ .github/init-deployment.js
@@ -6,7 +6,7 @@ try {
     .replace('<GRAPHQL-SERVER-IMAGE>', process.env.GRAPHQL_SERVER_IMAGE)
     .replace('<GRAPHQL-GATEWAY-IMAGE>', process.env.GRAPHQL_GATEWAY_IMAGE)
     .replace('<REPO-JOB-IMAGE>', process.env.REPO_JOB_IMAGE)
-    .replace('<CHANGE-CAUSE>', process.env.CHANGE_CAUSE)
+    .replace('<CHANGE-CAUSE>', `'${process.env.CHANGE_CAUSE.replaceAll("'", '')}'`)
     .replace('<GRAPHQL-GATEWAY-PORT', process.env.GRAPHQL_GATEWAY_PORT)
     .replace('<GRAPHQL-SERVER-PORT>', process.env.GRAPHQL_SERVER_PORT)
     .replace('<CERTIFICATE-ID>', process.env.CERTIFICATE_ID)

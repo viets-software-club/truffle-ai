aa71650892fc48326ee121457b1c27938ce0698f
Philipp Czernitzki <i@pczern.dev>
2023-06-16 12:27:46 +0200

    update _app.tsx - use new createPagesBrowserClient from Supabase - Authorization Bearer - env variables intead of BASE_URL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import { AppProps } from 'next/app'
 import { withUrqlClient } from 'next-urql'
 import { cacheExchange, fetchExchange } from 'urql'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import BASE_URL from '@/constants/baseUrl'
+import { getCookie } from '@/util/cookie'
 import '@/styles/globals.css'
 
 const App = ({
@@ -13,12 +13,7 @@ const App = ({
 }: AppProps<{
   initialSession: Session
 }>) => {
-  const [supabaseClient] = useState(() =>
-    createBrowserSupabaseClient({
-      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    })
-  )
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
 
   return (
     <SessionContextProvider
@@ -32,13 +27,19 @@ const App = ({
 
 export default withUrqlClient(
   () => ({
-    url: BASE_URL,
+    url: process.env.NEXT_PUBLIC_API_GRAPHQL_URL,
     exchanges: [cacheExchange, fetchExchange],
-    fetchOptions: () => ({
-      headers: {
-        Authorization: typeof window !== 'undefined' ? document.cookie : ''
-      }
-    })
+    fetchOptions: () => {
+      if (typeof window !== 'undefined') return {}
+      const cookie = getCookie(process.env.NEXT_PUBLIC_SUPABASE_AUTH_COOKIE_NAME)
+      if (cookie)
+        return {
+          headers: {
+            Authorization: `Bearer ${cookie}`
+          }
+        }
+      return {}
+    }
   }),
   { ssr: false }
 )(App)

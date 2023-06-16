de0d90f179e57c2f465bbf5bd0a2f58aece4a59c
Philipp Czernitzki <i@pczern.dev>
2023-06-16 12:40:13 +0200

    add login.ts

---
+++ src/util/login.ts
@@ -0,0 +1,29 @@
+import { validateEmail, validatePassword } from '@/util/validate'
+import { SupabaseClient } from '@supabase/auth-helpers-react'
+
+const redirectUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/`
+
+export const signInWithGoogle = async (supabaseClient: SupabaseClient<any, 'public', any>) => {
+  const { error } = await supabaseClient.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: redirectUrl
+    }
+  })
+  if (error) throw error
+}
+
+export const signInWithPassword = async (
+  supabaseClient: SupabaseClient<any, 'public', any>,
+  email: string,
+  password: string
+) => {
+  if (!validateEmail(email)) throw new Error("Couldn't validate email")
+  if (!validatePassword(password)) throw new Error("Couldn't validate password")
+
+  const { error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  })
+  if (error) throw error
+}

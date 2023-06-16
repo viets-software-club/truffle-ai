3e8c7235462b5185363d35d46a97cfb236a34267
Philipp Czernitzki <i@pczern.dev>
2023-06-16 13:06:49 +0200

    add user to context in server.ts

--- packages/graphql_server/src/server.ts
+++ packages/graphql_server/src/server.ts
@@ -1,8 +1,10 @@
-import Fastify from 'fastify'
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import mercurius from 'mercurius'
 import resolvers from './graphql/resolvers'
 import schema from './graphql/schema'
-
+import supbaseClient from './supabase'
+import fastifyCookies from '@fastify/cookie'
+import { UserResponse } from '@supabase/supabase-js'
 const ENV = process.env.NODE_ENV || 'development'
 
 const envToLogger = {
@@ -21,17 +23,36 @@ const envToLogger = {
 const app = Fastify({
   logger: envToLogger[ENV]
 })
-app.addHook('preHandler', function (req, reply, done) {
-  if (req.body) {
-    req.log.info({ body: req.body }, 'parsed body')
-  }
-  done()
+
+void app.register(fastifyCookies, {
+  hook: 'onRequest',
+  parseOptions: {}
 })
+
+const buildContext = async (request: FastifyRequest, reply: FastifyReply) => {
+  const jwt = request?.cookies?.jwt
+  if (jwt) {
+    const response: void | UserResponse = await supbaseClient.auth.getUser(jwt).catch((error) => {
+      reply.log.error(error)
+    })
+    if (response?.error) {
+      reply.log.error(response?.error)
+    }
+
+    return {
+      user: response?.data?.user
+    }
+  }
+  return null
+}
 
 // https://github.com/mercurius-js/mercurius-typescript/tree/master/examples
 void app.register(mercurius, {
   schema,
   resolvers,
+  context: buildContext,
+  // @TODO for security
+  // validationRules: process.env.NODE_ENV === 'production' ? [NoSchemaIntrospectionCustomRule] : [],
   graphiql: true // see http://localhost:3001/graphiql
 })
 export default app

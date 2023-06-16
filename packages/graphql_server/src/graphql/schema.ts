6d133ebb30046ae5782d63fb12ef34c458db8ace
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-06-16 10:12:09 +0300

    Schema addition

--- packages/graphql_server/src/graphql/schema.ts
+++ packages/graphql_server/src/graphql/schema.ts
@@ -2,10 +2,34 @@
 const schema = `
   type Query {
     helloWorld: String!
+    allProjects: [Project!]!
   }
   type Mutation {
     addProjectByName(name: String!, owner: String!): Boolean!
     addProjectByUrl(url: String!): Boolean!
+  }
+  type Project {
+    id: ID!
+    about: String!
+    eli5: String!
+    issue_count: Int!
+    fork_count: Int!
+    pull_request_count: Int!
+    contributor_count: Int!
+    github_url: String!
+    website_url: String!
+    owning_organization: ID!
+    is_bookmarked: Boolean!
+    is_trending_daily: Boolean!
+    is_trending_weekly: Boolean!
+    is_trending_monthly: Boolean!
+    star_history: JSON!
+    name: String!
+    languages: JSON!
+    owning_person: String!
+    hackernews_sentiment: String!
+    hackernews_stories: [String!]!
+    star_count: Int!
   }
 `
 

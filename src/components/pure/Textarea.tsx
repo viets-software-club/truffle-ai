580f164bdbb100496ad8cb79ecfb7141d1f0d242
Clemens <cnk1dev@gmail.com>
2023-06-16 21:24:00 +0200

    Add custom textarea component

---
+++ src/components/pure/Textarea.tsx
@@ -0,0 +1,13 @@
+import { DetailedHTMLProps, TextareaHTMLAttributes } from 'react'
+
+const Textarea = (
+  props: DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>
+) => (
+  <textarea
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
+    className="h-64 w-full min-w-[250px] rounded-[5px] border border-gray-800 bg-gray-900 px-3 py-2 text-14 text-white focus:outline focus:outline-indigo-500"
+  />
+)
+
+export default Textarea

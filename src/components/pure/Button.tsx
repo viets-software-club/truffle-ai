966e264166ed85da2a0fdc53314059c69fdce9d8
Philipp Czernitzki <i@pczern.dev>
2023-06-16 12:26:00 +0200

    Update Button.tsx

--- src/components/pure/Button.tsx
+++ src/components/pure/Button.tsx
@@ -8,13 +8,16 @@ type ButtonProps = {
     | 'onlyIconNoBorderNoBG'
     | 'normalHighlighted'
     | 'filter'
-  onClick: () => void
+  onClick?: () => void
   text?: string
   Icon?: IconComponentType
   order?: 'ltr' | 'rtl'
   iconColor?: string
   textColor?: string
   fullWidth?: boolean
+  type?: 'submit' | 'button'
+  name?: string
+  value?: string
 }
 
 const baseClassNames = `flex items-center rounded-[5px] transition-colors duration-100 hover:bg-gray-700`
@@ -38,7 +41,10 @@ const Button = ({
   order,
   iconColor = 'text-gray-500',
   textColor = 'text-gray-300',
-  fullWidth
+  fullWidth,
+  type,
+  name,
+  value
 }: ButtonProps) => {
   const classNames = `${baseClassNames} ${variantToButtonVariantClassNames.get(variant) ?? ''} ${
     fullWidth ? 'w-full' : ''
@@ -56,9 +62,14 @@ const Button = ({
       <Icon key="2" className={`${order === 'ltr' ? 'mr-1.5' : 'ml-1.5'} ${iconColor}`} />
     ))
   const contentNode = order === 'ltr' ? [iconNode, textNode] : [textNode, iconNode]
-
   return (
-    <button type="button" onClick={onClick} className={classNames}>
+    <button
+      type={type === 'button' ? 'button' : 'submit'}
+      onClick={onClick}
+      name={name}
+      value={value}
+      className={classNames}
+    >
       {contentNode}
     </button>
   )
@@ -70,7 +81,11 @@ Button.defaultProps = {
   order: 'rtl',
   iconColor: 'text-gray-500',
   textColor: 'text-gray-300',
-  fullWidth: false
+  fullWidth: false,
+  type: 'button',
+  onClick: null,
+  name: null,
+  value: null
 }
 
 export default Button

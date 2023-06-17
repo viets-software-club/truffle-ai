bb5ab2e324ff1d12b76c8d7f0ef2e3dbea0951b1
Philipp Czernitzki <i@pczern.dev>
2023-06-17 15:41:00 +0200

    update validate.ts - at least lower, upper letter and number

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -3,4 +3,8 @@ export const validateEmail = (email: string) => {
   return regex.test(email)
 }
 
-export const validatePassword = (password: string) => password.length > 6
+export const validatePassword = (password: string) =>
+  password.length >= 8 &&
+  password.search('/[a-z]/') > 0 &&
+  password.search('/[A-Z]/') > 0 &&
+  password.search('/[0-9]/') > 0

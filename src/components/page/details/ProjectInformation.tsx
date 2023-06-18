01191fe2be7922bc892211b4b13ea3f3c78be30d
yinruo <yinruoh@gmail.com>
2023-06-18 23:16:10 +0200

    add tootip

--- src/components/page/details/ProjectInformation.tsx
+++ src/components/page/details/ProjectInformation.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { FiBookmark as Bookmark } from 'react-icons/fi'
 import Button from '@/components/pure/Button'
+import { Tooltip } from 'react-tooltip'
 
 type ProjectInformationProps = {
   image: string
@@ -20,13 +21,16 @@ const ProjectInformation = ({ image, url, name, eli5, tags }: ProjectInformation
         <div className="relative mr-4 h-8 w-8 overflow-hidden rounded-[5px] bg-gray-600">
           <Image src={image} alt="logo" fill sizes="32px" />
         </div>
-
         <a href={url} target="_blank" rel="noreferrer">
-          <h1 className="mr-3 text-20 font-medium">
+          <h1 className="mr-3 text-20 font-medium" data-tooltip-id="project-url">
             {name.slice(0, 32)}
             {name.length > 32 ? '...' : ''}
           </h1>
         </a>
+
+        <Tooltip id="project-url" data-tooltip-content="Hello world!">
+          {url}
+        </Tooltip>
 
         {tags.length > 0 ? (
           tags.map((tag) => (

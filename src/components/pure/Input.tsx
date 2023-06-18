92d45b401459f0af076bd5c0fa83437387de6209
Clemens <cnk1dev@gmail.com>
2023-06-18 15:01:49 +0200

    Update input styling

--- src/components/pure/Input.tsx
+++ src/components/pure/Input.tsx
@@ -2,12 +2,18 @@ import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
 
 const Input = (
   props: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
-) => (
-  <input
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    {...props}
-    className="min-w-[250px] rounded-[5px] border border-gray-800 bg-gray-900 px-3 py-2 text-14 text-white focus:outline focus:outline-indigo-500"
-  />
-)
+) => {
+  const { className } = props
+
+  return (
+    <input
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      className={`min-w-[250px] rounded-[5px] border border-gray-800/75 bg-gray-900/50 px-3 py-2 text-14 text-white focus:outline focus:outline-indigo-500 ${
+        className || ''
+      }`}
+    />
+  )
+}
 
 export default Input

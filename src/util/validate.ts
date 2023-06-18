54c8c86ebfa9b3a4bae0848482cd87e2dd9144ea
Clemens <cnk1dev@gmail.com>
2023-06-18 13:27:08 +0200

    Update password policy to match Supabase

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -1,10 +1,5 @@
-export const validateEmail = (email: string) => {
-  const regex = /\S+@\S+\.\S+/
-  return regex.test(email)
-}
+const emailRegex = /\S+@\S+\.\S+/
 
-export const validatePassword = (password: string) =>
-  password.length >= 8 &&
-  password.search('/[a-z]/') > 0 &&
-  password.search('/[A-Z]/') > 0 &&
-  password.search('/[0-9]/') > 0
+export const validateEmail = (email: string) => emailRegex.test(email)
+
+export const validatePassword = (password: string) => password.length >= 8

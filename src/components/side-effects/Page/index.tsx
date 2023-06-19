22908a5eb69afbc14edb93475c9eb53b235ab498
Clemens <37487191+kigadye@users.noreply.github.com>
2023-06-19 14:06:28 +0200

    [META-87] Refactor command interface + improve UI (#71)

--- src/components/side-effects/Page/index.tsx
+++ src/components/side-effects/Page/index.tsx
@@ -1,58 +1,23 @@
-import { ReactNode, useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { ReactNode } from 'react'
 import NavSidebar from '@/components/side-effects/NavSidebar'
 import CommandInterface from '@/components/side-effects/CommandInterface'
-import defaultList from '../CommandInterface/DefaultRecommendationList'
 
 type PageProps = {
   children: ReactNode
 }
 
 /**
- * Standard page wrapper with sidebar and main element
+ * Standard page wrapper with sidebar, command interface and main content
  */
-const Page = ({ children }: PageProps) => {
-  const [openModal, setOpenModal] = useState<boolean>(false)
-  const router = useRouter()
+const Page = ({ children }: PageProps) => (
+  <div className="min-h-screen w-full bg-gray-900 text-gray-100">
+    <div className="flex">
+      <NavSidebar />
+      <CommandInterface />
 
-  useEffect(() => {
-    function handleKeyDown(event: KeyboardEvent) {
-      if (event.ctrlKey || event.metaKey) {
-        if (event.key === 'k') {
-          event.preventDefault()
-          setOpenModal(true)
-        }
-        const shortcutsForPages = defaultList.filter(
-          (item) => item.shortcutKey?.toLocaleLowerCase() === event.key.toLocaleLowerCase()
-        )
-        if (shortcutsForPages.length > 0) {
-          event.preventDefault()
-          void router.push(shortcutsForPages[0].commandInterfaceOptions)
-        }
-      } else if (event.key === 'Escape') {
-        setOpenModal(false)
-      }
-    }
-
-    document.addEventListener('keydown', handleKeyDown)
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [])
-  const closeModel = () => {
-    setOpenModal(false)
-  }
-
-  return (
-    <div className="min-h-screen w-full bg-gray-900 text-gray-100">
-      <div className="flex">
-        <NavSidebar />
-        {openModal && <CommandInterface action={closeModel} />}
-        <main className="ml-56 w-full text-gray-100">{children}</main>
-      </div>
+      <main className="ml-56 w-full text-gray-100">{children}</main>
     </div>
-  )
-}
+  </div>
+)
 
 export default Page

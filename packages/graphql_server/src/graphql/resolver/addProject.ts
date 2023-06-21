e5152acb65e4f7359117f69f8e258fc3947fc332
Tom Woehrle <thomas.e.woehrle@gmail.com>
2023-06-21 06:00:51 +0200

    Let manually added repos get bookmarked straight away

--- packages/graphql_server/src/graphql/resolver/addProject.ts
+++ packages/graphql_server/src/graphql/resolver/addProject.ts
@@ -1,6 +1,7 @@
 import supabase from '../../supabase'
 import { getOrganizationID, getPersonID, repoIsAlreadyInDB } from '../../supabaseUtils'
 import { updateAllProjectInfo } from '../../updateProject'
+import { addBookmark } from './bookmark'
 
 export const addProject = async (repoName: string, owner: string, bookmarkCategory: string) => {
   if (await repoIsAlreadyInDB(repoName, owner)) {
@@ -17,6 +18,7 @@ export const addProject = async (repoName: string, owner: string, bookmarkCatego
       // update all the data sources. trending state may be null
       // no await so that the return happens faster
       void updateAllProjectInfo(repoName, owner, null)
+      void addBookmark(repoName, owner, bookmarkCategory)
       return true
     }
   }

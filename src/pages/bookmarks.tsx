092688877960da46c83e1a681b4268f8d19f5a01
Clemens <cnk1dev@gmail.com>
2023-06-22 13:36:31 +0200

    update bookmarks page to show actual bookmarks

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -1,10 +1,57 @@
-import ProjectsTable from '@/components/side-effects/ProjectsTable'
+import { useState, useEffect } from 'react'
+import { useUser } from '@supabase/auth-helpers-react'
 import Page from '@/components/side-effects/Page'
 import withAuth from '@/components/side-effects/withAuth'
+import ProjectsTable from '@/components/side-effects/ProjectsTable'
+import { defaultFilters, defaultSort } from '@/components/page/overview/types'
+import {
+  Project,
+  ProjectFilter,
+  ProjectOrderBy,
+  useAllBookmarksQuery
+} from '@/graphql/generated/gql'
 
-const Bookmarks = () => (
-  <Page>
-    <ProjectsTable />
-  </Page>
-)
+// @TODO add category column to table
+
+/**
+ * Project table with all bookmarks of a user
+ */
+const Bookmarks = () => {
+  const [data, setData] = useState<Project[]>([])
+  const [filters, setFilters] = useState<ProjectFilter>(defaultFilters)
+  const [sorting, setSorting] = useState<ProjectOrderBy | null>(defaultSort)
+
+  const user = useUser()
+
+  const updateFilters = (filter: ProjectFilter) => {
+    setFilters(filter)
+  }
+
+  // Fetch data from Supabase using generated Urql hook
+  const [{ data: urqlData, fetching, error }] = useAllBookmarksQuery({
+    variables: { userId: user?.id as string }
+  })
+
+  // Only update table data when urql data changes
+  useEffect(() => {
+    if (urqlData) {
+      setData(urqlData?.bookmarkCollection?.edges?.map((edge) => edge.node.project) as Project[])
+    }
+  }, [urqlData])
+
+  return (
+    <Page>
+      <ProjectsTable
+        data={data}
+        filters={filters}
+        sorting={sorting}
+        fetching={fetching}
+        error={error}
+        setSorting={setSorting}
+        updateFilters={updateFilters}
+      />
+    </Page>
+  )
+}
+
 export default withAuth(Bookmarks)

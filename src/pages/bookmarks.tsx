ca95718e2c8698d7ad8ae0dbeec0ae40d37972e6
Clemens <cnk1dev@gmail.com>
2023-06-23 00:39:22 +0200

    make filtering & sorting work on bookmarks page

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -8,7 +8,8 @@ import {
   Project,
   ProjectFilter,
   ProjectOrderBy,
-  useAllBookmarksQuery
+  useBookmarkIdsQuery,
+  useTrendingProjectsQuery
 } from '@/graphql/generated/gql'
 
 // @TODO add category column to table
@@ -27,15 +28,31 @@ const Bookmarks = () => {
     setFilters(filter)
   }
 
-  // Fetch data from Supabase using generated Urql hook
-  const [{ data: urqlData, fetching, error }] = useAllBookmarksQuery({
-    variables: { userId: user?.id as string }
-  })
+  const [{ data: bookmarkData, fetching: fetchingBookmarks, error: errorBookmarks }] =
+    useBookmarkIdsQuery({ variables: { userId: user?.id as string } })
+
+  // Get array with all bookmarked project ids
+  const bookmarkIds = bookmarkData?.bookmarkCollection?.edges?.map(
+    (edge) => edge.node.project?.id as string
+  ) as string[]
+
+  const [{ data: urqlData, fetching: fetchingProjects, error: errorProjects }] =
+    useTrendingProjectsQuery({
+      variables: {
+        orderBy: sorting || defaultSort,
+        filter: {
+          ...(filters || defaultFilters),
+          id: {
+            in: bookmarkIds
+          }
+        }
+      }
+    })
 
   // Only update table data when urql data changes
   useEffect(() => {
     if (urqlData) {
-      setData(urqlData?.bookmarkCollection?.edges?.map((edge) => edge.node.project) as Project[])
+      setData(urqlData?.projectCollection?.edges?.map((edge) => edge.node) as Project[])
     }
   }, [urqlData])
 
@@ -45,8 +62,9 @@ const Bookmarks = () => {
         data={data}
         filters={filters}
         sorting={sorting}
-        fetching={fetching}
-        error={error}
+        fetching={fetchingProjects || fetchingBookmarks}
+        error={errorProjects || errorBookmarks}
+        hideTimeFrame
         setSorting={setSorting}
         updateFilters={updateFilters}
       />

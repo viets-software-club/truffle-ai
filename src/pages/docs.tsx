85f80a163f0eaba858d9f1923af20d0b5b7dd3cc
Lukas <ge64diw@mytum.de>
2023-06-30 00:42:31 +0200

    fix mistake

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -398,9 +398,9 @@ const Docs = () => {
 
           <Paragraph>
             All our pages are protected, which means that if the user tries to access any page
-            without being logged in, they will be prompted to log in by this screen: If the user
-            enters wrong login credentials or tries to log in using an email or a Google account
-            that has not been whitelisted, they will receive an error message.
+            without being logged in, they will be prompted to log in by this screen. <br /> If the
+            user enters wrong login credentials or tries to log in using an email or a Google
+            account that has not been whitelisted, they will receive an error message.
           </Paragraph>
 
           <HeaderTwo text="Navbar" />

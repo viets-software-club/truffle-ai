2cfa4c8ca208d5d1f7b74ea101bce4120ae21214
Lukas <ge64diw@mytum.de>
2023-06-30 00:26:12 +0200

    Add Settings

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -481,16 +481,86 @@ const Docs = () => {
           />
 
           <HeaderTwo text="Project Details" />
-          <Paragraph> </Paragraph>
+          <Paragraph>
+            The Project Details page gives the user an in-depth look at an individual project. This
+            page presents them with all the necessary information they need to understand the
+            project, its growth, its social media traction, and its creator(s).{' '}
+          </Paragraph>
+          <HeaderThree text="Navigation" />
+          <Paragraph>
+            At the top of the Details page, the user will find three navigation buttons:
+          </Paragraph>
+          <NormalBulletList
+            items={[
+              'Close button: Closes the Details page and takes them back to the Trending Projects page.',
+              'Arrow Up: Takes the user to the previous project based on the list they are currently going through (WIP).',
+              'Arrow Down: Takes the user to the next project.'
+            ]}
+          />
+
+          <HeaderThree text="Main Panel" />
+          <Paragraph>
+            The main panel of the Details page is packed with useful information about the project:
+          </Paragraph>
+          <NormalBulletList
+            items={[
+              'Logo',
+              'Project owner and name',
+              'Categories: One or more predefined categories under which the project falls.',
+              'About: original project description from GitHub',
+              'ELI5 description: A simple explanation of what the project is about.',
+              'Bookmark: This allows you to bookmark the project for future reference. Once clicked a modal opens up, asking the user to enter the name of the category it should be bookmarked in.',
+              'Send to Slack: This enables the user to share the project directly to a Slack channel using the webhook URL provided through the settings (see below).',
+              "Star/ forks history: This interactive chart shows the growth in the project's star count over time.",
+              'Top Tweets: List of the most popular tweets related to the project.',
+              'Hackernews sentiment: Summary of the sentiment of the top comments from Hackernews.'
+            ]}
+          />
+
+          <HeaderThree text="Right Sidebar" />
+          <Paragraph>
+            The right sidebar provides statistics and social media information about the project and
+            its founders:
+          </Paragraph>
+          <NormalBulletList
+            items={[
+              "GitHub Stats: These are the project's stats on GitHub, such as stars, issues, forks, and contributors, as well as combined metrics like issues/ contributor or forks/ contributor.",
+              'Languages: These are the programming languages used.',
+              'Company: If Truffle was able to find a LinkedIn account or a website, the links are provided here.',
+              "Founder: This area provides information about the project's founders, including their name, email, website, GitHub, Twitter, Website, and count of repositories.",
+              "Integrations: The 'Send to Affinity' button allows the user to add the project details to their CRM for further tracking and outreach beyond Truffle."
+            ]}
+          />
 
           <HeaderTwo text="All Bookmarks" />
-          <Paragraph> </Paragraph>
+          <Paragraph>
+            This page gives the user an overview of all projects which were bookmarked regardless of
+            their category.
+          </Paragraph>
 
           <HeaderTwo text="Compare Categories" />
-          <Paragraph> </Paragraph>
+          <Paragraph>
+            This page helps the user compare projects bookmarked into the same category. The user
+            can access this page by clicking on one of the folders on the left, which represents the
+            different categories the projects belong to.
+            <br />
+            The user can filter and sort the entries similar to the Trending Projects page.
+          </Paragraph>
+          <NormalBulletList
+            items={[
+              'Send to Slack: This button sends a notification with a list of all the projects in the category to Slack.',
+              'Edit category: This enables the user to rename the category.',
+              'Select timeframe: This allows the user to display only values in the specific timeframe in the chart.',
+              'Normalize Data: This button is used to synchronize the starting point of all datasets to the earliest date for comparison purposes. It toggles between the original and the normalized state of the data.',
+              'Stars: The user can choose whether to display the stars growth or the forks growth in the chart.'
+            ]}
+          />
 
           <HeaderTwo text="Settings" />
-          <Paragraph> </Paragraph>
+          <Paragraph>
+            The page is divided into three sections: General, Account, and Integrations.{' '}
+          </Paragraph>
+          <HeaderThree text="General" />
 
           <HeaderTwo text="Command Interface" />
           <Paragraph> </Paragraph>

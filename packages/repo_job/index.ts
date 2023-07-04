444d707768220e4433ae47828a9d74e1ebe9b75b
Tom Woehrle <thomas.e.woehrle@gmail.com>
2023-07-04 13:48:11 +0200

    Add graphql endpoint to repo job for testing purposes

--- packages/repo_job/index.ts
+++ packages/repo_job/index.ts
@@ -8,6 +8,12 @@ setTimeout(() => {
   //   .then((response) => console.log(response.data))
 
   void axios
+    .post(process.env.SERVER_GRAPHQL_URL as string, { query: '{ helloWorld }' })
+    .then((response) => {
+      console.log(response.data)
+    })
+
+  void axios
     .get((process.env.SERVER_URL as string) + '/test')
     .then((response) => console.log(response.data))
 }, 10000)

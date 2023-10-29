2a08f703b8945e2c7fe7d50495bb14788184f884
Clemens <cnk1dev@gmail.com>
2023-10-29 19:11:48 +0100

    Refactor bookmark modal

--- packages/ui/src/components/pure/Modal.tsx
+++ packages/ui/src/components/pure/Modal.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react'
+import { Fragment, ReactNode } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 
 type ModalProps = {
   isOpen: boolean
@@ -6,34 +7,38 @@ type ModalProps = {
   children: ReactNode
 }
 
-/**
- * Simple modal (incl. overlay)
- */
-const Modal = ({ isOpen, onClose, children }: ModalProps) => {
-  if (!isOpen) return null
+const Modal = ({ isOpen, onClose, children }: ModalProps) => (
+  <Transition appear show={isOpen} as={Fragment}>
+    <Dialog as='div' onClose={onClose} className='fixed inset-0 z-30'>
+      <Transition.Child
+        as={Fragment}
+        enter='ease-out duration-200'
+        enterFrom='opacity-0'
+        enterTo='opacity-100'
+        leave='ease-in duration-200'
+        leaveFrom='opacity-100'
+        leaveTo='opacity-0'>
+        <div className='fixed inset-0 bg-gray-950/25' />
+      </Transition.Child>
 
-  return (
-    <>
-      {/* Overlay: a semi-transparent black background that covers the whole page */}
-      <div
-        className='fixed inset-0 z-40 bg-black opacity-10'
-        onClick={onClose}
-        onKeyDown={event => {
-          if (event.key === 'Enter' || event.key === ' ') {
-            onClose()
-          }
-        }}
-        tabIndex={0}
-        // Treat the overlay as a button for accessibility purposes
-        role='button'
-      />
-
-      {/* Modal */}
-      <div className='z-50 m-0'>
-        <div className='absolute bg-gray-850'>{children}</div>
+      <div className='fixed inset-0 flex items-center justify-center'>
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-200'
+          enterFrom='opacity-0 scale-95'
+          enterTo='opacity-100 scale-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100 scale-100'
+          leaveTo='opacity-0 scale-95'>
+          <Dialog.Panel
+            as='div'
+            className='flex min-w-[350px] flex-col gap-4 rounded-lg border border-gray-900/20 bg-gray-700 p-4 text-left shadow-lg shadow-gray-950/10'>
+            {children}
+          </Dialog.Panel>
+        </Transition.Child>
       </div>
-    </>
-  )
-}
+    </Dialog>
+  </Transition>
+)
 
 export default Modal

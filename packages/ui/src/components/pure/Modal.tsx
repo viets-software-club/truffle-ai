f16e3da6dd3c1a89cb7d401924142cd6216b92f2
Clemens <cnk1dev@gmail.com>
2023-10-29 19:19:01 +0100

    Reuse generic modal

--- packages/ui/src/components/pure/Modal.tsx
+++ packages/ui/src/components/pure/Modal.tsx
@@ -1,21 +1,23 @@
 import { Fragment, ReactNode } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import clsx from 'clsx'
 
 type ModalProps = {
   isOpen: boolean
   onClose: () => void
   children: ReactNode
+  unstyled?: boolean
 }
 
-const Modal = ({ isOpen, onClose, children }: ModalProps) => (
+const Modal = ({ isOpen, onClose, children, unstyled }: ModalProps) => (
   <Transition appear show={isOpen} as={Fragment}>
     <Dialog as='div' onClose={onClose} className='fixed inset-0 z-30'>
       <Transition.Child
         as={Fragment}
-        enter='ease-out duration-200'
+        enter='ease-out duration-150'
         enterFrom='opacity-0'
         enterTo='opacity-100'
-        leave='ease-in duration-200'
+        leave='ease-in duration-150'
         leaveFrom='opacity-100'
         leaveTo='opacity-0'>
         <div className='fixed inset-0 bg-gray-950/25' />
@@ -24,15 +26,18 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => (
       <div className='fixed inset-0 flex items-center justify-center'>
         <Transition.Child
           as={Fragment}
-          enter='ease-out duration-200'
+          enter='ease-out duration-150'
           enterFrom='opacity-0 scale-95'
           enterTo='opacity-100 scale-100'
-          leave='ease-in duration-200'
+          leave='ease-in duration-150'
           leaveFrom='opacity-100 scale-100'
           leaveTo='opacity-0 scale-95'>
           <Dialog.Panel
             as='div'
-            className='flex min-w-[350px] flex-col gap-4 rounded-lg border border-gray-900/20 bg-gray-700 p-4 text-left shadow-lg shadow-gray-950/10'>
+            className={clsx(
+              !unstyled &&
+                'flex min-w-[400px] flex-col gap-4 rounded-lg border border-gray-900/20 bg-gray-700 p-6 text-left shadow-lg shadow-gray-950/10'
+            )}>
             {children}
           </Dialog.Panel>
         </Transition.Child>
--- packages/ui/src/components/side-effects/CommandInterface/CommandInterfaceModal.tsx
+++ packages/ui/src/components/side-effects/CommandInterface/CommandInterfaceModal.tsx
@@ -1,5 +1,5 @@
 import { FC, FormEvent, RefObject } from 'react'
-import { Transition, Dialog } from '@headlessui/react'
+import Modal from '@/components/pure/Modal'
 import RecommendationRow from './RecommendationRow'
 import { RecommendationRowType } from './types'
 
@@ -23,9 +23,6 @@ type CommandInterfaceModalProps = {
   handleSubmit: (event: FormEvent<HTMLFormElement>) => void
 }
 
-/**
- * Command interface UI
- */
 const CommandInterfaceModal: FC<CommandInterfaceModalProps> = ({
   open,
   selectedLine,
@@ -40,86 +37,59 @@ const CommandInterfaceModal: FC<CommandInterfaceModalProps> = ({
   handleChange,
   handleSubmit
 }) => (
-  <Transition appear show={open}>
-    <Dialog
-      as='div'
-      open={open}
-      onClose={toggleModal}
-      className='fixed inset-0 z-30 flex h-screen w-screen justify-center'>
-      <Transition.Child
-        enter='ease-out duration-300'
-        enterFrom='opacity-0'
-        enterTo='opacity-100'
-        leave='ease-in duration-200'
-        leaveFrom='opacity-100'
-        leaveTo='opacity-0'>
-        <div className='fixed inset-0 bg-gray-950/25' />
-      </Transition.Child>
+  <Modal isOpen={open} onClose={toggleModal} unstyled>
+    <div
+      ref={wrapperRef}
+      className='-mt-28 max-h-[450px] w-[650px] overflow-hidden rounded-lg border-[0.5px] border-gray-700/75 bg-gray-100/5 backdrop-blur-2xl transition-all duration-300'>
+      <form onSubmit={handleSubmit}>
+        <input
+          className='block w-full appearance-none bg-transparent px-6 py-5 text-lg font-light text-gray-300 outline-none placeholder:text-gray-500'
+          onChange={handleChange}
+          value={searchWord}
+          autoComplete='off'
+          spellCheck='false'
+          type='text'
+          id='spotlight'
+          ref={inputRef}
+          placeholder='Type a command or search...'
+        />
+      </form>
 
-      <Transition.Child
-        enter='transition ease-out duration-100'
-        enterFrom='transform opacity-0 scale-95'
-        enterTo='transform opacity-100 scale-100'
-        leave='transition ease-in duration-75'
-        leaveFrom='transform opacity-100 scale-100'
-        leaveTo='transform opacity-0 scale-95'>
-        <Dialog.Panel
-          as='div'
-          ref={wrapperRef}
-          className='mt-28 max-h-[450px] w-[650px] overflow-hidden rounded-lg border-[0.5px] border-gray-700/75 bg-gray-100/5 backdrop-blur-2xl transition-all duration-300'>
-          <form onSubmit={handleSubmit}>
-            <input
-              className='block w-full appearance-none bg-transparent px-6 py-5 text-lg font-light text-gray-300 outline-none placeholder:text-gray-500'
-              onChange={handleChange}
-              value={searchWord}
-              autoComplete='off'
-              spellCheck='false'
-              type='text'
-              id='spotlight'
-              ref={inputRef}
-              placeholder='Type a command or search...'
+      <div className='border-b-[0.5px] border-gray-500/25' />
+
+      <div
+        ref={listRef}
+        className='h-full max-h-[383px] w-full overflow-y-auto py-2 scrollbar-hide'>
+        {recommendationList &&
+          recommendationList.map((item, index) => (
+            <RecommendationRow
+              key={item.menuText}
+              Icon={item.Icon}
+              isHighlighted={index === selectedLine}
+              isNextItem={
+                selectedLine + 1 === recommendationList.length ? true : selectedLine + 1 === index
+              }
+              isPrevItem={
+                selectedLine === 0 && index + 1 === recommendationList.length
+                  ? true
+                  : selectedLine === index + 1
+              }
+              menuText={item.menuText}
+              subtitle={item.subtitle}
+              isProjectItem={isProjectListOn}
+              shortcutKey={item.shortcutKey}
+              handleClick={() =>
+                handleClick(
+                  item.commandInterfaceOptions,
+                  item.menuText,
+                  item.isIdPrimary ?? false,
+                  item.needConfirmation ?? false
+                )
+              }
             />
-          </form>
-
-          <div className='border-b-[0.5px] border-gray-500/25' />
-
-          <div
-            ref={listRef}
-            className='h-full max-h-[383px] w-full overflow-y-auto py-2 scrollbar-hide'>
-            {recommendationList &&
-              recommendationList.map((item, index) => (
-                <RecommendationRow
-                  key={item.menuText}
-                  Icon={item.Icon}
-                  isHighlighted={index === selectedLine}
-                  isNextItem={
-                    selectedLine + 1 === recommendationList.length
-                      ? true
-                      : selectedLine + 1 === index
-                  }
-                  isPrevItem={
-                    selectedLine === 0 && index + 1 === recommendationList.length
-                      ? true
-                      : selectedLine === index + 1
-                  }
-                  menuText={item.menuText}
-                  subtitle={item.subtitle}
-                  isProjectItem={isProjectListOn}
-                  shortcutKey={item.shortcutKey}
-                  handleClick={() =>
-                    handleClick(
-                      item.commandInterfaceOptions,
-                      item.menuText,
-                      item.isIdPrimary ?? false,
-                      item.needConfirmation ?? false
-                    )
-                  }
-                />
-              ))}
-          </div>
-        </Dialog.Panel>
-      </Transition.Child>
-    </Dialog>
-  </Transition>
+          ))}
+      </div>
+    </div>
+  </Modal>
 )
 export default CommandInterfaceModal

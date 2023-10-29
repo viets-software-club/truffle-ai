08c807ef766d7f2c7e5ed08998270e4a054719b3
czer <philip@czer.eu>
2023-10-29 21:57:32 +0100

    Update

---
+++ config/kubernetes/build-single.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# -----------
+# required arguments:
+# OUT_DIR for the yml file
+# APP = truffle
+# NAMESPACE = production | staging | commit
+# ENVIRONMENT = production | staging
+# PREFIX = production | staging | commit-<hash>
+# CERTIFICATE_ID = (from DigitalOcean)
+# CHANGE_CAUSE = 
+# UI_PORT
+# UI_IMAGE
+# GRAPHQL_GATEWAY_PORT
+# GRAPHQL_GATEWAY_IMAGE
+# GRAPHQL_SERVER_IMAGE
+# GRAPHQL_SERVER_PORT
+# REPO_CRONJOB_IMAGE
+
+mkdir ./deployment;
+touch ./deployment/$1;
+env envsubst < $1 > ./deployment/$1.yml;

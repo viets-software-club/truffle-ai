89208cafd8ac5f60cee29a4ba0cd7abd8da5b6d1
Clemens <cnk1dev@gmail.com>
2023-11-02 21:48:07 +0100

    Update Dockerfiles

--- packages/preview_job/destroy_pods_ns_commit.sh
+++ packages/preview_job/destroy_pods_ns_commit.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # destroy every pod in namespace commit after one day
 doctl kubernetes cluster kubeconfig save --expiry-seconds 600 $DO_KUBE_CLUSTER_NAME
 kubectl get pods --namespace=commit -o go-template --template '{{range .items}}{{.metadata.name}} {{.metadata.creationTimestamp}}{{"\n"}}{{end}}' | awk '$2 <= "'$(date -d 'yesterday' -Ins --utc | sed 's/+0000/Z/')'" { print $1 }' | xargs --no-run-if-empty kubectl delete pod

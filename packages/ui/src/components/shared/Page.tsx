da6af529f29aed2fa7db36bcbc9a168ca09ecc81
Clemens <cnk1dev@gmail.com>
2023-11-04 03:16:07 +0100

    Make compare page responsive

--- packages/ui/src/components/shared/Page.tsx
+++ packages/ui/src/components/shared/Page.tsx
@@ -9,7 +9,7 @@ const Page = ({ children }: PropsWithChildren) => (
   <div className='flex min-h-screen w-full text-white/90'>
     <NavSidebar />
     <CommandInterface />
-    <main className='w-full max-w-[100vw] text-white/90 md:max-w-[calc(100vw-48px)] lg:ml-56 lg:max-w-[calc(100vw-224px-48px)]'>
+    <main className='w-full max-w-[100vw] text-white/90 lg:ml-56 lg:max-w-[calc(100vw-224px)]'>
       {children}
     </main>
   </div>

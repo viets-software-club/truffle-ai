cd30cf87d5c8a71adf37713de71f1703df762a80
Clemens <cnk1dev@gmail.com>
2023-11-05 17:00:50 +0100

    Small UI improvements

--- packages/ui/src/components/domain/settings/AffinitySettings.tsx
+++ packages/ui/src/components/domain/settings/AffinitySettings.tsx
@@ -19,32 +19,39 @@ const AffinitySettings = () => {
   }
 
   return (
-    <>
-      <p className='pb-2 text-sm text-white/75'>Affinity API token</p>
-      <Input
-        type='password'
-        id='affinity'
-        name='affinity'
-        placeholder='•••••••••••••••••'
-        value={affinityKey}
-        onChange={e => setAffinityKey(e.target.value)}
-      />
-      <p className='mt-4 pb-2 text-sm text-white/75'>Affinity List ID</p>
-      <Input
-        type='text'
-        id='listId'
-        name='listId'
-        placeholder='Enter your List ID here'
-        value={listId}
-        onChange={e => setListId(e.target.value)}
-      />
-      <div className='mt-6'>
+    <div className='flex flex-col items-stretch gap-4 sm:items-start'>
+      <div className='flex w-full flex-col gap-2 sm:w-auto'>
+        <p className='text-sm text-white/75'>Affinity API token</p>
+        <Input
+          type='password'
+          id='affinity'
+          name='affinity'
+          placeholder='•••••••••••••••••'
+          value={affinityKey}
+          onChange={e => setAffinityKey(e.target.value)}
+        />
+      </div>
+
+      <div className='flex w-full flex-col gap-2 sm:w-auto'>
+        <p className='text-sm text-white/75'>Affinity List ID</p>
+        <Input
+          type='text'
+          id='listId'
+          name='listId'
+          placeholder='Enter your List ID here'
+          value={listId}
+          onChange={e => setListId(e.target.value)}
+        />
+      </div>
+
+      <div className='mt-2'>
         <Button variant='highlighted' size='large' onClick={showBannerFunc}>
           Update
         </Button>
-        {showBanner && <Banner variant='success' message='Values updated' />}
       </div>
-    </>
+
+      {showBanner && <Banner variant='success' message='Values updated' />}
+    </div>
   )
 }
 
--- packages/ui/src/components/shared/Modal.tsx
+++ packages/ui/src/components/shared/Modal.tsx
@@ -36,7 +36,7 @@ const Modal = ({ isOpen, onClose, children, unstyled }: ModalProps) => (
             as='div'
             className={clsx(
               !unstyled &&
-                'flex w-[400px] max-w-[94vw] flex-col gap-4 rounded-xl border border-white/5 bg-white/5 p-6 text-left shadow-sm shadow-white/5 backdrop-blur-xl'
+                'flex w-[400px] max-w-[94vw] flex-col gap-4 rounded-xl border border-white/5 bg-white/5 p-4 text-left shadow-sm shadow-white/5 backdrop-blur-xl md:p-6'
             )}>
             {children}
           </Dialog.Panel>

0a4501e9b4419e666bb325c02dff73bc089ec865
czer <philip@czer.eu>
2023-11-07 03:27:09 +0100

    update

--- config/terraform/modules/kubernetes-config/main.tf
+++ config/terraform/modules/kubernetes-config/main.tf
@@ -1,7 +1,3 @@
-data "digitalocean_kubernetes_cluster" "this" {
-  name = var.cluster_name
-}
-
 resource "local_file" "kubeconfig" {
   count    = var.write_kubeconfig ? 1 : 0
   content  = data.digitalocean_kubernetes_cluster.this.kube_config[0].raw_config

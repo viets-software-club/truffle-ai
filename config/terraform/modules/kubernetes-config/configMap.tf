a810ba657f6c51ff182ede9dd83aec27ed973707
czer <philip@czer.eu>
2023-11-07 23:06:50 +0100

    update

--- config/terraform/modules/kubernetes-config/configMap.tf
+++ config/terraform/modules/kubernetes-config/configMap.tf
@@ -1,5 +1,5 @@
 data "kubectl_file_documents" "docs" {
-  content = file("${path.module}/configMaps.${terraform.workspace}.yml")
+  content = file("${path.module}/configMaps/configMaps.${terraform.workspace}.yml")
 }
 
 resource "kubectl_manifest" "test" {

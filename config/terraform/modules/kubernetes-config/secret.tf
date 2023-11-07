555fbadf3ade427806d0c00971822cf103aa0e30
czer <philip@czer.eu>
2023-11-08 00:52:26 +0100

    update

---
+++ config/terraform/modules/kubernetes-config/secret.tf
@@ -0,0 +1,80 @@
+resource "kubernetes_secret" "github_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-github-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+    GITHUB_API_TOKEN = var.secret_github_api_token
+  }
+}
+resource "kubernetes_secret" "graphql_gateway_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-graphql-gateway-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+  }
+}
+resource "kubernetes_secret" "graphql_server_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-graphql-server-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+  }
+}
+resource "kubernetes_secret" "open_api_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-open-api-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+    OPEN_API_KEY = var.secret_open_api_key
+  }
+}
+resource "kubernetes_secret" "preview_job_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-preview-job-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+  }
+}
+resource "kubernetes_secret" "repo_job_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-repo-job-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+  }
+}
+resource "kubernetes_secret" "scraping_bot_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-scraping-bot-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+    SCRAPING_BOT_USER_NAME = var.secret_scraping_bot_user_name
+    SCRAPING_BOT_API_KEY   = var.secret_scraping_bot_api_key
+  }
+}
+resource "kubernetes_secret" "supabase_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-supabase-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+    SUPABASE_ANON_KEY    = var.secret_supabase_anon_key
+    SUPABASE_SERVICE_KEY = var.secret_supabase_service_key
+  }
+}
+resource "kubernetes_secret" "ui_secret" {
+  metadata {
+    name      = "${var.namespace_prefix}-ui-secret"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  data = {
+    NEXT_PUBLIC_SUPABASE_ANON_KEY = var.secret_supabase_anon_key
+  }
+}
+

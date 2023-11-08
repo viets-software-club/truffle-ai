3ebd7f68c609579e12d0c684b37bf9de7f0f4a79
czer <philip@czer.eu>
2023-11-08 15:16:21 +0100

    update

--- config/terraform/modules/kubernetes-config/provider.tf
+++ config/terraform/modules/kubernetes-config/provider.tf
@@ -30,15 +30,22 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  host = data.digitalocean_kubernetes_cluster.this.endpoint
-  # token = data.digitalocean_kubernetes_cluster.this.kube_config[0].token
+  host  = data.digitalocean_kubernetes_cluster.this.endpoint
+  token = data.digitalocean_kubernetes_cluster.this.kube_config[0].token
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
   )
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "doctl"
-    args = ["kubernetes", "cluster", "kubeconfig", "exec-credential",
-    "--version=v1beta1", data.digitalocean_kubernetes_cluster.this.id]
-  }
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   command     = "doctl"
+  #   args = ["kubernetes", "cluster", "kubeconfig", "exec-credential",
+  #   "--version=v1beta1", data.digitalocean_kubernetes_cluster.this.id]
+  # }
+}
+
+provider "kubectl" {
+  host                   = data.digitalocean_kubernetes_cluster.this.endpoint
+  token                  = data.digitalocean_kubernetes_cluster.this.kube_config[0].token
+  cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
+  load_config_file       = false
 }

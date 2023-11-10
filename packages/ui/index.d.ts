e2dac66e43958a711664e6ba241c5ae5ef4d364a
czer <philip@czer.eu>
2023-11-10 05:49:08 +0100

    update

--- packages/ui/index.d.ts
+++ packages/ui/index.d.ts
@@ -7,8 +7,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string
-    NEXT_PUBLIC_SITE_URL: string
-    NEXT_PUBLIC_API_URL: string
     NEXT_PUBLIC_API_GRAPHQL_URL: string
   }
 }

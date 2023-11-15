e38c6119164f31b1168dc12ffe6e41043d2cdb7c
Philipp D. Czernitzki <philip@czer.eu>
2023-11-15 23:37:58 +0000

    update

--- config/scripts/add-app.ts
+++ config/scripts/add-app.ts
@@ -11,7 +11,7 @@ let addCmd = new Deno.Command('helm', {
 })
 
 let add = await addCmd.output()
-console.log(new TextDecoder().decode(add.stdout))
+console.log('added helm repo', new TextDecoder().decode(add.stdout))
 let updateCmd = new Deno.Command('helm', { args: ['repo', 'update'] })
 let update = await updateCmd.output()
-console.log(new TextDecoder().decode(update.stdout))
+console.log('updated helm repos', new TextDecoder().decode(update.stdout))

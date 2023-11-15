59c7c06b9ab2e8fee155c420f7f2b325544a3e41
Philipp D. Czernitzki <philip@czer.eu>
2023-11-15 23:45:08 +0000

    update

--- config/scripts/deploy-app.ts
+++ config/scripts/deploy-app.ts
@@ -1,4 +1,5 @@
 const REPO_NAME = Deno.env.get('REPO_NAME')
+const ORG_NAME = Deno.env.get('REPO_NAME')
 
 const environment = prompt('Which environment (commit)') || Deno.env.get('NAMESPACE', 'commit')
 const sha = prompt('What commit sha?') || Deno.env.get('GIT_COMMIT_TAG', null)

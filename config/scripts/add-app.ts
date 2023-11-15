f81c3e801c235269f035c44fcec0e4b6e16fc40c
Philipp D. Czernitzki <philip@czer.eu>
2023-11-15 23:36:08 +0000

    update

--- config/scripts/add-app.ts
+++ config/scripts/add-app.ts
@@ -1,12 +1,17 @@
 const ORG_NAME = Deno.env.get('ORG_NAME')
 const REPO_NAME = Deno.env.get('REPO_NAME')
 const GITHUB_TOKEN = Deno.env.get('GITHUB_TOKEN')
-let addCmd = new Deno.Command(`helm repo add ${ORG_NAME}`, {
-  args: [`https://raw.githubusercontent.com/${ORG_NAME}/${REPO_NAME}/main/`]
+let addCmd = new Deno.Command('helm', {
+  args: [
+    'repo',
+    'add',
+    ORG_NAME,
+    `https://raw.githubusercontent.com/${ORG_NAME}/${REPO_NAME}/main/`
+  ]
 })
 
 let add = await addCmd.output()
 console.log(new TextDecoder().decode(add.stdout))
-let updateCmd = new Deno.Command('helm repo update')
+let updateCmd = new Deno.Command('helm', { args: ['repo', 'update'] })
 let update = await updateCmd.output()
 console.log(new TextDecoder().decode(update.stdout))
--- config/scripts/deploy-app.ts
+++ config/scripts/deploy-app.ts
@@ -1,43 +1,46 @@
-const REPO_NAME = Deno.env.get("REPO_NAME")
+const REPO_NAME = Deno.env.get('REPO_NAME')
 
-const environment = prompt("Which environment (commit)") || Deno.env.get("NAMESPACE", "commit");
-const sha = prompt("What commit sha?") || Deno.env.get("GIT_COMMIT_TAG", null)
-const promptedChangeCause = prompt("What is the change cause?") || ""
+const environment = prompt('Which environment (commit)') || Deno.env.get('NAMESPACE', 'commit')
+const sha = prompt('What commit sha?') || Deno.env.get('GIT_COMMIT_TAG', null)
+const promptedChangeCause = prompt('What is the change cause?') || ''
 const hasTag = !!sha
-let certificateId = prompt("What is the certificate uuid (auto-filled via doctl)"); // doctl compute certificate list --format ID --no-header
-const isSure = confirm("Are you sure about your settings?")
-if(!isSure) Deno.exit("Goodbye")
+let certificateId = prompt('What is the certificate uuid (auto-filled via doctl)') // doctl compute certificate list --format ID --no-header
+const isSure = confirm('Are you sure about your settings?')
+if (!isSure) Deno.exit('Goodbye')
 
-const shortSha = hasTag ? sha.substring(0,7) : ''
-const NAMESPACE = hasTag ? `${environment}-${shortSha}` : environment;
-const RESOURCE_PREFIX = NAMESPACE;
+const shortSha = hasTag ? sha.substring(0, 7) : ''
+const NAMESPACE = hasTag ? `${environment}-${shortSha}` : environment
+const RESOURCE_PREFIX = NAMESPACE
 const CHANGE_CAUSE = hasTag ? `${shortSha}: ${promptedChangeCause}` : promptedChangeCause
-const certificateCmd = new Deno.Command("doctl compute certificate list", ["--format ID", "--no-header"])
-if(!certificateId) {
-    let { code, stdout, stderr } = await certificateCmd.output();
-    certificateId = new TextDecoder().decode(stdout)
+const certificateCmd = new Deno.Command('doctl compute certificate list', [
+  '--format ID',
+  '--no-header'
+])
+if (!certificateId) {
+  let { code, stdout, stderr } = await certificateCmd.output()
+  certificateId = new TextDecoder().decode(stdout)
 }
 
 const IMAGE_REPOSITORY_URL = `ghcr.io/${ORG_NAME}/${REPO_NAME}/${environment}`
 const IMAGE_TAG = sha
 
 const args = [
-    `--set nginx-ingress-controller.service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-name=${REPO_NAME}-nginx-ingress-controller`,
-    `--set nginx-ingress-controller.service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-certificate-id=${certificateId}`,
-    `--set image.repositoryUrl=${IMAGE_REPOSITORY_URL}`,
-    `--set image.tag=${IMAGE_TAG}`,
-    `--namespace ${NAMESPACE}`,
-    `--set resPrefix=${RESOURCE_PREFIX}`,
-    '--atomic',
-    '--install',
-    '--create-namespace',
-    `--namespace=${NAMESPACE}`
+  'upgrade',
+  `--set nginx-ingress-controller.service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-name=${REPO_NAME}-nginx-ingress-controller`,
+  `--set nginx-ingress-controller.service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-certificate-id=${certificateId}`,
+  `--set image.repositoryUrl=${IMAGE_REPOSITORY_URL}`,
+  `--set image.tag=${IMAGE_TAG}`,
+  `--namespace ${NAMESPACE}`,
+  `--set resPrefix=${RESOURCE_PREFIX}`,
+  '--atomic',
+  '--install',
+  '--create-namespace',
+  `--namespace=${NAMESPACE}`
 ]
 
+let deployCmd = new Deno.Command('helm', { args })
 
-let deployCmd = new Deno.Command("helm upgrade", { args });
-
-let { code, stdout, stderr } = await deployCmd.output();
+let { code, stdout, stderr } = await deployCmd.output()
 
 // stdout & stderr are a Uint8Array
-console.log(new TextDecoder().decode(stdout));
+console.log(new TextDecoder().decode(stdout))

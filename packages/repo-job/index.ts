b56f886075147599a819b5cd2f573f11c1575c4f
Philipp D. Czernitzki <philip@czer.eu>
2023-11-16 22:40:49 +0000

    update

--- packages/repo-job/index.ts
+++ packages/repo-job/index.ts
@@ -3,11 +3,7 @@ import axios from 'axios'
 console.log('Repo job run at', new Date().toLocaleString())
 
 void axios
-  .get(
-    `http://truffle-ai-graphql-backend-deployment:${
-      process.env.GRAPHQL_SERVER_PORT as string
-    }/updateDatabase`
-  )
+  .get(`http://graphql-backend-service:${process.env.GRAPHQL_SERVER_PORT as string}/updateDatabase`)
   .then((response) => {
     console.log(response.data)
   })

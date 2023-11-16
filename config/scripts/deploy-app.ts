029891d81518c55e8ae7d22abd0f7eca57322adb
Philipp D. Czernitzki <philip@czer.eu>
2023-11-16 16:56:43 +0000

    update

--- config/scripts/deploy-app.ts
+++ config/scripts/deploy-app.ts
@@ -14,13 +14,13 @@ if (!isSure) Deno.exit('Goodbye')
 const shortSha = hasTag ? sha.substring(0, 7) : ''
 const NAMESPACE = hasTag ? `${environment}-${shortSha}` : environment
 const RESOURCE_PREFIX = NAMESPACE
-const CHANGE_CAUSE = hasTag ? `${shortSha}: ${promptedChangeCause}` : promptedChangeCause
+const CHANGE_CAUSE = hasTag ? `${shortSha}-${promptedChangeCause}` : promptedChangeCause
 const certificateCmd = new Deno.Command('doctl', {
   args: ['compute', 'certificate', 'list', '--format', 'ID', '--no-header']
 })
 if (!certificateId) {
   let { code, stdout, stderr } = await certificateCmd.output()
-  certificateId = new TextDecoder().decode(stdout)
+  certificateId = new TextDecoder().decode(stdout).replaceAll('\n', '')
   stderr && console.error(new TextDecoder().decode(stderr))
 }
 
@@ -33,12 +33,11 @@ const HOST =
     ? 'truffle.tools'
     : 'staging.truffle.tools'
 console.log('res', RESOURCE_PREFIX)
+
 const args = [
   'upgrade',
-  '--set',
-  `nginx-ingress-controller.service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-name="${REPO_NAME}-nginx-ingress-controller"`,
-  '--set',
-  `nginx-ingress-controller.service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-certificate-id="${certificateId}"`,
+  '--set-json',
+  `'nginx-ingress-controller.service.annotations={"service.beta.kubernetes.io/do-loadbalancer-name": "${REPO_NAME}-nginx-ingress-controller", "service.beta.kubernetes.io/do-loadbalancer-certificate-id": "${certificateId}"}'`,
   '--set',
   `image.repositoryUrl=${IMAGE_REPOSITORY_URL}`,
   '--set',
@@ -63,8 +62,8 @@ if (promptedVersion) {
   args.push('--version', promptedVersion)
 }
 args.push(`chart-${RESOURCE_PREFIX}`, `${ORG_NAME}/app-chart`)
-
-let deployCmd = new Deno.Command('helm', { args, stdin: 'piped' })
+console.log('Deploy Command:\n', 'helm', args.join(' '))
+// let deployCmd = new Deno.Command('helm', { args })
 
 // let { code, stdout, stderr } = await deployCmd.output()
 

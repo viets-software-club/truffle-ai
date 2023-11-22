eabe85f27b34965d358a9dfe3d28fbe548fd7863
Philipp D. Czernitzki <philip@czer.eu>
2023-11-22 02:52:48 +0000

    Delete repo

--- config/scripts/helm/repo/add.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zx
-// adds repository to helm
-import 'zx/globals'
-
-$`helm repo add truffle-ai-stable --username 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 --password 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 oci://ghcr.io/viets-software-club/truffle-ai/stable/app-chart`
--- config/scripts/helm/repo/update.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zx
-// updates helm repo specified by $RELEASE_REPO_NAME
-import 'zx/globals'
-
-$`helm repo update truffle-ai-stable`

4d759e3b3b3934ef3f665d235710517cc5661208
Clemens <cnk1dev@gmail.com>
2023-11-25 09:22:51 +0100

    Fix mobile menu not closing bug

--- packages/ui/src/components/domain/sidebar/MobileNavLink.tsx
+++ packages/ui/src/components/domain/sidebar/MobileNavLink.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link'
 
-type MobileNavLinkProps = { Icon: IconComponentType; text: string; path: string }
+type MobileNavLinkProps = {
+  Icon: IconComponentType
+  text: string
+  path: string
+  onClick?: () => void
+}
 
-const MobileNavLink = ({ Icon, text, path }: MobileNavLinkProps) => (
+const MobileNavLink = ({ Icon, text, path, onClick }: MobileNavLinkProps) => (
   <Link
     className='flex items-center gap-2 rounded-md px-2 py-3 text-sm transition-colors duration-150 hover:bg-white/5'
-    href={path}>
+    href={path}
+    onClick={onClick}>
     <Icon className='opacity-50' />
     {text}
   </Link>

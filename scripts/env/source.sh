73770eb2a60d9606a3006cd8fdd84ca77e2185fe
czer <philip@czer.eu>
2023-12-23 05:04:24 +0100

    change source.sh to include path to root as arg & update Taskfile

--- scripts/env/source.sh
+++ scripts/env/source.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-export $(cat .env.common | grep -v \"#\" | xargs);
+arg1=${1:-"."}
+export $(cat $arg1/.env.common | grep -v \"#\" | xargs);
 export GIT_COMMIT_TAG=$(git rev-parse HEAD);
 if [ "$ENVIRONMENT" = "production" ] 
 then
 zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.production')}`);
 EOF
-cp .env.production ./packages/ui/.env.local;
-export $(cat .env.production | grep -v \"#\" | xargs);
+cp $arg1/.env.production $arg1/packages/ui/.env.local;
+export $(cat $arg1/.env.production | grep -v \"#\" | xargs);
 elif [ "$ENVIRONMENT" = "staging" ] 
 then
 zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.staging')}`);
 EOF
-cp .env.staging ./packages/ui/.env.local;
-export $(cat .env.staging | grep -v \"#\" | xargs);
+cp $arg1/.env.staging $arg1/packages/ui/.env.local;
+export $(cat $arg1/.env.staging | grep -v \"#\" | xargs);
 else
 zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.commit')}`);
 EOF
 export ENVIRONMENT=commit;
-cp .env.commit ./packages/ui/.env.local;
-export $(cat .env.commit | grep -v \"#\" | xargs);
+cp $arg1/.env.commit $arg1/packages/ui/.env.local;
+export $(cat $arg1/.env.commit | grep -v \"#\" | xargs);
 fi;

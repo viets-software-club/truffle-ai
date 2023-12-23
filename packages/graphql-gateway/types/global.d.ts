c9998db7238473241240f06926cae2d363e1db3d
czer <philip@czer.eu>
2023-12-23 03:16:13 +0100

    Add jwt secret on gateway

--- packages/graphql-gateway/types/global.d.ts
+++ packages/graphql-gateway/types/global.d.ts
@@ -1,9 +1,10 @@
-export {}
+export { }
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       SUPABASE_URL: string
       SUPABASE_SERVICE_KEY: string
+      SUPABASE_JWT_SECRET: string
       NODE_ENV: 'development' | 'production'
       PORT: number
     }

026951f1778c86d8077be8523262c953346b8c4d
czer <philip@czer.eu>
2023-12-29 09:55:36 +0100

    Update source env scripts

--- scripts/env/source-and-run.sh
+++ scripts/env/source-and-run.sh
@@ -3,21 +3,21 @@ export $(cat .env.common | grep -v \"#\" | xargs);
 export GIT_COMMIT_TAG=$(git rev-parse HEAD);
 if [ "$ENVIRONMENT" = "production" ] 
 then
-zx << 'EOF'
+./node_modules/.bin/zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.production')}`);
 EOF
 cp .env.production ./packages/ui/.env.local;
 export $(cat .env.production | grep -v \"#\" | xargs);
 elif [ "$ENVIRONMENT" = "staging" ] 
 then
-zx << 'EOF'
+./node_modules/.bin/zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.staging')}`);
 EOF
 cp .env.staging ./packages/ui/.env.local;
 export $(cat .env.staging | grep -v \"#\" | xargs);
 else
 export ENVIRONMENT=commit;
-zx << 'EOF'
+./node_modules/.bin/zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.commit')}`);
 EOF
 cp .env.commit ./packages/ui/.env.local;
--- scripts/env/source.sh
+++ scripts/env/source.sh
@@ -4,20 +4,20 @@ export $(cat $arg1/.env.common | grep -v \"#\" | xargs);
 export GIT_COMMIT_TAG=$(git rev-parse HEAD);
 if [ "$ENVIRONMENT" = "production" ] 
 then
-zx << 'EOF'
+./node_modules/.bin/zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.production')}`);
 EOF
 cp $arg1/.env.production $arg1/packages/ui/.env.local;
 export $(cat $arg1/.env.production | grep -v \"#\" | xargs);
 elif [ "$ENVIRONMENT" = "staging" ] 
 then
-zx << 'EOF'
+./node_modules/.bin/zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.staging')}`);
 EOF
 cp $arg1/.env.staging $arg1/packages/ui/.env.local;
 export $(cat $arg1/.env.staging | grep -v \"#\" | xargs);
 else
-zx << 'EOF'
+./node_modules/.bin/zx << 'EOF'
 echo(`sourcing ${chalk.cyan.bold('.env.commit')}`);
 EOF
 export ENVIRONMENT=commit;

ce251653f036614979367329ea17dbf02f2f84eb
czer <philip@czer.eu>
2024-01-02 20:02:24 +0100

    format with biome

--- packages/graphql-gateway/src/server.ts
+++ packages/graphql-gateway/src/server.ts
@@ -166,10 +166,13 @@ app.route({
 	}
 })
 
-void app.listen({ port: process.env.GRAPHQL_GATEWAY_PORT, host: '0.0.0.0' }, (err, address) => {
-	if (err) {
-		app.log.error(err)
-		process.exit(1)
+void app.listen(
+	{ port: process.env.GRAPHQL_GATEWAY_PORT, host: '0.0.0.0' },
+	(err, address) => {
+		if (err) {
+			app.log.error(err)
+			process.exit(1)
+		}
+		app.log.info(`server listening on ${address}`)
 	}
-	app.log.info(`server listening on ${address}`)
-})
+)

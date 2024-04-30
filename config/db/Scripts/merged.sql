fe128f04b18f6f9fc832662ebe6b3dab8beeaa9e
czer <philip@czer.eu>
2024-04-30 05:09:09 +0200

    add f_list_gthb_repos_by_cat_title

--- config/db/Scripts/merged.sql
+++ config/db/Scripts/merged.sql
@@ -1744,6 +1744,21 @@ begin
 end;
 $$
 language plpgsql stable;
+drop function if exists f_list_gthb_repos_by_cat_title(text);
+create or replace function f_list_gthb_repos_by_cat_title(cat_title_arg text)
+  returns setof "gthb_repo"
+  as $$
+begin
+  return query select gthb_repo.* from gthb_repo
+  inner join proj_repo on proj_repo.gthb_repo_id = gthb_repo.gthb_repo_id
+  inner join proj_bookmark on proj_bookmark.proj_repo_id = proj_repo.proj_repo_id
+  inner join proj_cat_and_proj_bookmark on proj_cat_and_proj_bookmark.proj_bookmark_id = proj_bookmark.proj_bookmark_id
+  inner join proj_cat on proj_cat.proj_cat_id = proj_cat_and_proj_bookmark.proj_cat_id
+  where proj_cat.title = cat_title_arg and auth.uid() = proj_cat.auth_users_id;
+end;
+$$
+language plpgsql stable;
+
 create or replace trigger tr_on_delete_delete_unreferenced_algo_hn_query
   after delete on proj_repo_and_algo_hn_query
   for each STATEMENT

f129ac917a63ed028bf50a3aac030cf614aa2cd3
Philipp D. Czernitzki <philip@czer.eu>
2024-05-19 15:13:52 +0200

    revert back to sveltekit-superforms 12.4

--- packages/ui/src/lib/components/impure/LoginForm.svelte
+++ packages/ui/src/lib/components/impure/LoginForm.svelte
@@ -107,7 +107,7 @@
 </form>
 <div class="relative">
 	<div class="absolute inset-0 flex items-center">
-		<span class="w-full border-t" />
+		<span class="w-full border-t"></span>
 	</div>
 	<div class="relative flex justify-center text-xs uppercase">
 		<span class="bg-background px-2 text-muted-foreground"> Or continue with </span>
--- packages/ui/src/lib/supabase/index.ts
+++ packages/ui/src/lib/supabase/index.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-console.log(PUBLIC_SUPABASE_URL);
 const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
 export { supabaseClient };

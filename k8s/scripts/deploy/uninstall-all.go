e00eae7e11f70128880f8b98a34ec5ca356c5afb
Philipp D. Czernitzki <philip@czer.eu>
2024-06-19 00:58:56 +0200

    Update uninstsall-all.go

--- k8s/scripts/deploy/uninstall-all.go
+++ k8s/scripts/deploy/uninstall-all.go
@@ -27,14 +27,23 @@ func main() {
 	}
 
 	for _, deployment := range deployments {
+		fmt.Println(deployment)
 		cmd1 := exec.Command("helm", "uninstall", deployment.Name, "--namespace", deployment.Namespace)
 		cmd1.Stdout = os.Stdout
 		cmd1.Stderr = os.Stderr
-		
+		err = cmd1.Run()
+		if err != nil {
+			log.Println(err)
+		}
 
 		cmd2 := exec.Command("kubectl", "delete", "namespace", deployment.Namespace)
 		cmd2.Stdout = os.Stdout
 		cmd2.Stderr = os.Stderr
+		err = cmd2.Run()
+
+		if err != nil {
+			log.Println(err)
+		}
 		
 		
 		

9a5512174f4a2cb93d8da5cd949ba4ead35c4863
Philipp D. Czernitzki <philip@czer.eu>
2024-06-18 13:07:52 +0200

    add golang gateway

---
+++ packages/gateway/go.mod
@@ -0,0 +1,5 @@
+module github.com/viets-software-club/truffle-ai/auth_server
+
+go 1.22.2
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
---
+++ packages/gateway/main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type AuthUsersIdResponse struct {
+	AuthUsersId string `json:"auth_users_id"`
+}
+
+type UserResponse struct {
+	Id          string      `json:"id"`
+	Aud         string      `json:"aud"`
+	Role        string      `json:"role"`
+	Email       string      `json:"email"`
+	AppMetadata AppMetadata `json:"app_metadata"`
+}
+type AppMetadata struct {
+	Provider  string   `json:"provider"`
+	Providers []string `json:"providers"`
+}
+
+func fetchAuthUsersIdFromSupabase(userApiKey string) (*AuthUsersIdResponse, error) {
+	encodedUserApiKey := url.QueryEscape(userApiKey)
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/rest/v1/user_api_key?select=auth_users_id&user_api_key=eq.%s&limit=1", os.Getenv("SUPABASE_URL"), encodedUserApiKey), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("apikey", os.Getenv("SUPABASE_SERVICE_KEY"))
+	req.Header.Add("authorization", fmt.Sprintf("Bearer %s", os.Getenv("SUPABASE_SERVICE_KEY")))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var authUserIdResponse AuthUsersIdResponse
+	err = json.Unmarshal(body, &authUserIdResponse)
+	if err != nil {
+		return nil, err
+	}
+	if authUserIdResponse.AuthUsersId == "" {
+		return nil, fmt.Errorf("auth_users_id not found")
+	}
+
+	return &authUserIdResponse, nil
+}
+
+func fetchUserFromSupabaseAsAdmin(authUsersId string) (*UserResponse, error) {
+	encodedAuthUsersId := url.QueryEscape(authUsersId)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/auth/v1/admin/users/%s", os.Getenv("SUPABASE_URL"), encodedAuthUsersId), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("apikey", os.Getenv("SUPABASE_SERVICE_KEY"))
+	req.Header.Add("authorization", fmt.Sprintf("Bearer %s", os.Getenv("SUPABASE_SERVICE_KEY")))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var userResponse UserResponse
+	err = json.Unmarshal(body, &userResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &userResponse, nil
+}
+
+func fetchUserFromSupabase(authorizationHeader string) (*UserResponse, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/auth/v1/user/", os.Getenv("SUPABASE_URL")), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("apikey", os.Getenv("SUPABASE_ANON_KEY"))
+	req.Header.Add("authorization", authorizationHeader)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var userResponse UserResponse
+	err = json.Unmarshal(body, &userResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &userResponse, nil
+}
+
+func main() {
+
+	supabaseGraphqlUrl, err := url.Parse(os.Getenv("SUPABASE_GRAPHQL_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	customServerGraphqlUrl, err := url.Parse(os.Getenv("GRAPHQL_SERVER_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("cu GraphQL URL: %s\n", customServerGraphqlUrl)
+	supabaseGraphqlProxy := httputil.NewSingleHostReverseProxy(supabaseGraphqlUrl)
+	customServerGraphqlProxy := httputil.NewSingleHostReverseProxy(customServerGraphqlUrl)
+	customServerGraphqlProxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+        log.Printf("Error proxying request: %v", err)
+        http.Error(w, "Error proxying request", http.StatusInternalServerError)
+    }
+	customServerGraphqlProxy.ModifyResponse = func(r *http.Response) error {
+		r.Header.Set("Access-Control-Allow-Origin", "*")
+        r.Header.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        r.Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-server, apikey")
+		
+		log.Printf("Response: %d %s", r.StatusCode, r.Status)
+	
+		// Log all response headers
+		for name, values := range r.Header {
+			// Loop over all values for the name.
+			for _, value := range values {
+				log.Printf("%s: %s", name, value)
+			}
+		}
+	
+		return nil
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Printf("Received %s request for %s\n", r.Method, r.URL)
+        // fmt.Println("Headers:")
+        // for name, values := range r.Header {
+        //     // Loop over all values for the name.
+        //     for _, value := range values {
+        //         fmt.Printf("%s: %s\n", name, value)
+        //     }
+        // }
+	
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+        	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-server, apikey")
+		
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
+		xServerHeader := r.Header.Get("X-Server")
+		authorizationHeader := r.Header.Get("Authorization")
+		userApiKeyHeader := r.Header.Get("userApiKey")
+
+		if strings.HasPrefix(authorizationHeader, "Bearer ") && len(authorizationHeader) > 7 {
+			if xServerHeader == "supabase-graphql" {
+				http.Redirect(w, r, os.Getenv("SUPABASE_GRAPHQL_URL"), http.StatusFound)
+				return
+			} else if xServerHeader == "server" {
+				resp, err := fetchUserFromSupabase(authorizationHeader)
+				if err != nil {
+					log.Println(err)
+					w.WriteHeader(http.StatusUnauthorized)
+				}
+				fmt.Printf("%+v\n", resp)
+				r.Header.Set("authusersid", resp.Id)
+				fmt.Println("check")
+				customServerGraphqlProxy.ServeHTTP(w, r)
+				return;
+			} else {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+		}
+
+		if userApiKeyHeader != "" {
+			autherUserIdResp, err := fetchAuthUsersIdFromSupabase(userApiKeyHeader)
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			userResp, err := fetchUserFromSupabaseAsAdmin(autherUserIdResp.AuthUsersId)
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+				"sub":          userResp.Id,
+				"iss":          fmt.Sprintf("%s/auth/v1", os.Getenv("SUPABASE_URL")),
+				"aud":          userResp.Aud,
+				"role":         userResp.Role,
+				"email":        userResp.Email,
+				"app_metadata": userResp.AppMetadata,
+			})
+
+			tokenString, err := token.SignedString([]byte(os.Getenv("SUPABASE_JWT_SECRET")))
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			r.Header.Set("authorization", fmt.Sprintf("Bearer %s", tokenString))
+			r.Header.Set("authusersid", userResp.Id)
+
+			if xServerHeader == "supabase-graphql" {
+				supabaseGraphqlProxy.ServeHTTP(w, r)
+				return
+			} else if xServerHeader == "server" {
+				customServerGraphqlProxy.ServeHTTP(w, r)
+				return
+			} else {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusUnauthorized)
+	})
+
+	fmt.Printf("Starting server on port %s\n", os.Getenv("GATEWAY_PORT"))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("GATEWAY_PORT")), nil))
+}

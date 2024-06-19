909b608f50a84bf7650391ccec7174e73d9610b5
Philipp D. Czernitzki <philip@czer.eu>
2024-06-19 23:58:54 +0200

    Update db

--- db/sql/supabase_realtime_publication.sql
+++ db/sql/supabase_realtime_publication.sql
@@ -7,9 +7,13 @@ create publication supabase_realtime;
 
 commit;
 
-alter
-  publication supabase_realtime add table proj_bookmark;
+-- alter
+--   publication supabase_realtime add table proj_bookmark;
 alter
   publication supabase_realtime add table proj_cat;
-alter table proj_bookmark replica identity full;
+alter
+  publication supabase_realtime add table proj_cat_and_proj_bookmark;
+-- alter table proj_bookmark replica identity full;
 alter table proj_cat replica identity full;
+alter table proj_cat_and_proj_bookmark replica identity full;
+

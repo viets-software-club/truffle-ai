456401273c960c1eb1dbcebe4871a64617971dbc
Philipp D. Czernitzki <philip@czer.eu>
2024-06-20 05:31:20 +0200

    Update k8s

--- k8s/scripts/deploy/release.go
+++ k8s/scripts/deploy/release.go
@@ -17,7 +17,7 @@ import (
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter the version for deployment as it was tagged on GitHub e.g. v1.0.0")
+	fmt.Println("Enter the version for deployment as it was tagged on GitHub without 'v' e.g. 1.0.0")
 	promptVersion, _ := reader.ReadString('\n')
 
 	// Is it a dry run, print output to command line without deploying?
@@ -35,14 +35,16 @@ func main() {
 		"--namespace",
 		"public",
 		"--set",
-		fmt.Sprintf("image.repositoryUrl=\"ghcr.io/%s/%s/stable\"", os.Getenv("ORG_NAME"), os.Getenv("REPO_NAME")),
+		fmt.Sprintf("image.repositoryUrl=ghcr.io/%s/%s/stable", os.Getenv("ORG_NAME"), os.Getenv("REPO_NAME")),
+		"--set",
+		fmt.Sprintf("image.tag=%s", strings.TrimSpace(promptVersion)),
 		"--set-json",
 		fmt.Sprintf("hosts=%s", hostsJson),
 		"--values",
 		"./values/values.production.yml",
 		"--cleanup-on-fail",
 		"--version",
-		strings.TrimSpace(promptVersion),
+		strings.TrimSpace("v" + promptVersion),
 	}
 	if isDryRun {
 		args = append(args, "--dry-run")
